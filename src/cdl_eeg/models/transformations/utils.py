6777baf0cd90be5ab9508a40521a93808e612cd7
thomas <thomas.tveitstol@gmail.com>
2023-08-21 10:06:02 +0200

    added function and made unit test

--- src/cdl_eeg/models/transformations/utils.py
+++ src/cdl_eeg/models/transformations/utils.py
@@ -1,16 +1,17 @@
 import numpy
 
 
-def chunk_eeg(data, k, chuck_duration, delta_t):
-    """Function for splitting
+def chunk_eeg(*, data, k, chunk_duration, delta_t):
+    """Function for splitting EEG data into chunks
 
+    (unittest in test folder)
     Parameters
     ----------
     data : numpy.ndarray
         EEG data with shape=(num_subjects, num_channels, num_timesteps)
     k : int
         Number of EEG chunks
-    chuck_duration : int
+    chunk_duration : int
         The duration of the EEG chunks (in number of timesteps)
 
     delta_t : int:
@@ -22,3 +23,12 @@ def chunk_eeg(data, k, chuck_duration, delta_t):
         The EEG data split into chunks
 
     """
+    # Add the first chuck
+    chunk = [data[..., 0:chunk_duration]]
+
+    # Append all the other chunks
+    for i in range(1, k):
+        i0 = i * (delta_t + chunk_duration)
+        chunk.append(data[..., i0:(i0+chunk_duration)])
+
+    return tuple(chunk)
---
+++ tests/models/transformations/test_utils.py
@@ -0,0 +1,30 @@
+import numpy
+
+from cdl_eeg.models.transformations.utils import chunk_eeg
+
+
+def test_chuck_eeg():
+    """Test if the chunk_eeg function works properly"""
+    # ------------------------
+    # Generate data todo: config file
+    # ------------------------
+    data = tuple(numpy.ones(shape=(10, 25, 300)) * (j + 1) for j in range(4))
+    intervals = tuple(numpy.zeros(shape=(10, 25, 200)) for _ in range(3))
+
+    # Stack them
+    chunked_data = [data[0]]
+    for interval, chunk in zip(intervals, data[1:]):
+        chunked_data.append(interval)
+        chunked_data.append(chunk)
+
+    chunked_data = numpy.concatenate(chunked_data, axis=-1)
+
+    # ------------------------
+    # Compare actual with expected
+    # ------------------------
+    actual_chunks = chunk_eeg(data=chunked_data, k=4, chunk_duration=300, delta_t=200)
+
+    assert len(actual_chunks) == len(data), (f"The number of chunks was wrong. Expected {len(data)}, got "
+                                             f"{len(actual_chunks)}")
+    assert all(numpy.array_equal(actual, expected) for actual, expected in zip(actual_chunks, data)), \
+        "The arrays were not as expected"
