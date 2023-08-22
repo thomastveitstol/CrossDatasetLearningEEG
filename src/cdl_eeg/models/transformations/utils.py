6c8213c3c7fb8f8fbfc9f5bc3c5762c681f2bf99
thomas <thomas.tveitstol@gmail.com>
2023-08-22 11:53:05 +0200

    made classes for random sampling

--- src/cdl_eeg/models/transformations/utils.py
+++ src/cdl_eeg/models/transformations/utils.py
@@ -1,3 +1,7 @@
+import abc
+
+import numpy.random
+
 
 def chunk_eeg(*, data, k, chunk_duration, delta_t):
     """Function for splitting EEG data into chunks
@@ -30,3 +34,110 @@ def chunk_eeg(*, data, k, chunk_duration, delta_t):
         chunk.append(data[..., i0:(i0+chunk_duration)])
 
     return tuple(chunk)
+
+
+# -------------------------
+# Random number classes
+# -------------------------
+class RandomBase(abc.ABC):
+
+    @abc.abstractmethod
+    def draw(self, seed=None):
+        """
+        Draw a sample from the distribution
+
+        Parameters
+        ----------
+        seed : int
+            A seed can be passed for reproducibility
+
+        Returns
+        -------
+        A sample from the distribution
+        """
+
+
+class UnivariateNormal(RandomBase):
+    """
+    Class for drawing samples from a univariate normal distribution. To reproduce, numpy.random.seed must be called
+    """
+
+    __slots__ = "_mean", "_std"
+
+    def __init__(self, mean=0, std=1):
+        """
+        Initialisation method
+
+        Parameters
+        ----------
+        std : float
+            Standard deviation of the normal distribution
+        mean : float
+            Mean of the normal distribution
+        """
+        # ------------------
+        # Set attributes
+        # ------------------
+        self._mean = mean
+        self._std = std
+
+    def draw(self, seed=None):
+        """
+        Examples
+        -------
+        >>> UnivariateNormal(3, 0.5).draw(seed=1)  # doctest: +ELLIPSIS
+        3.812...
+
+        It is the same to set seed outside, as passing the seed to the method
+
+        >>> numpy.random.seed(1)
+        >>> UnivariateNormal(3, 0.5).draw()  # doctest: +ELLIPSIS
+        3.812...
+        """
+        # Maybe set seed
+        if seed is not None:
+            numpy.random.seed(seed)
+
+        # Draw a sample from the distribution and return
+        return numpy.random.normal(loc=self._mean, scale=self._std)
+
+
+class UnivariateUniform(RandomBase):
+
+    __slots__ = "_lower", "_upper"
+
+    def __init__(self, lower, upper):
+        """
+        Initialisation
+
+        Parameters
+        ----------
+        lower : float
+            Lower bound for the uniform distribution
+        upper : float
+            Upper bound for the uniform distribution
+        """
+        # ------------------
+        # Set attributes
+        # ------------------
+        self._lower = lower
+        self._upper = upper
+
+    def draw(self, seed=None):
+        """
+        Examples:
+        >>> UnivariateUniform(-1, 1).draw(seed=1)  # doctest: +ELLIPSIS
+        -0.165...
+
+        It is the same to set seed outside, as passing the seed to the method
+
+        >>> numpy.random.seed(1)
+        >>> UnivariateUniform(-1, 1).draw()  # doctest: +ELLIPSIS
+        -0.165...
+        """
+        # Maybe set seed
+        if seed is not None:
+            numpy.random.seed(seed)
+
+        # Draw a sample from the distribution and return
+        return numpy.random.uniform(low=self._lower, high=self._upper)
