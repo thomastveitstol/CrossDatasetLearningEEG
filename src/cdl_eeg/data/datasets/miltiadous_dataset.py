cc3941c0b643181c82bd122cdc3e3a97550cc19f
thomas <thomas.tveitstol@gmail.com>
2023-08-30 13:57:05 +0200

    added method for loading MNE object

--- src/cdl_eeg/data/datasets/miltiadous_dataset.py
+++ src/cdl_eeg/data/datasets/miltiadous_dataset.py
@@ -1,3 +1,7 @@
+import os
+
+import mne
+
 from cdl_eeg.data.datasets.dataset_base import EEGDatasetBase, ChannelSystemBase
 
 
@@ -9,3 +13,11 @@ class Miltiadous(EEGDatasetBase):
 
     def __init__(self):
         super().__init__(channel_system=MiltiadousChannelSystem())
+
+    def load_single_mne_object(self, subject_id):
+        # TODO: add derivatives functionality
+        # Create path
+        path = os.path.join(self.get_mne_path(), subject_id, "eeg", f"{subject_id}_task-eyesclosed_eeg.set")
+
+        # Load MNE object and return
+        return mne.io.read_raw_eeglab(input_fname=path, preload=True)
