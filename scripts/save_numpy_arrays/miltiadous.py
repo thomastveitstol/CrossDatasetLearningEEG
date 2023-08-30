92b076b94dcea1c1ce7a77e6a2350f2013310cd2
thomas <thomas.tveitstol@gmail.com>
2023-08-30 14:11:33 +0200

    improve script (mypy+flake8)

---
+++ scripts/save_numpy_arrays/miltiadous.py
@@ -0,0 +1,26 @@
+from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+
+
+def main():
+    # ----------------
+    # Hyperparameters  todo: currently running as testing/debugging only
+    # ----------------
+    # Pre-processing
+    filtering = (1, 40)
+    resample = 500
+    notch_filter = 50
+
+    # Cropping
+    num_time_steps = resample * 16
+    time_series_start = resample * 10
+
+    # ----------------
+    # Saving
+    # ----------------
+    Miltiadous().save_eeg_as_numpy_arrays(subject_ids=None, filtering=filtering, resample=resample,
+                                          notch_filter=notch_filter, num_time_steps=num_time_steps,
+                                          time_series_start=time_series_start)
+
+
+if __name__ == "__main__":
+    main()
