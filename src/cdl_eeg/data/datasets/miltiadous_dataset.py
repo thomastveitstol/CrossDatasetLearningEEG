5774b7fa0425312a7d76d224e60cdf27e4181596
thomas <thomas.tveitstol@gmail.com>
2023-09-05 14:26:33 +0200

    kwargs input handling and todo

--- src/cdl_eeg/data/datasets/miltiadous_dataset.py
+++ src/cdl_eeg/data/datasets/miltiadous_dataset.py
@@ -20,14 +20,15 @@ class Miltiadous(EEGDatasetBase):
         """
         super().__init__(channel_system=MiltiadousChannelSystem())
 
-    def _load_single_raw_mne_object(self, subject_id):
+    def _load_single_raw_mne_object(self, subject_id, **_):
+        # todo: there have to be better ways of handling the input data than **_
         # Create path
         path = os.path.join(self.get_mne_path(), subject_id, "eeg", f"{subject_id}_task-eyesclosed_eeg.set")
 
         # Load MNE object and return
         return mne.io.read_raw_eeglab(input_fname=path, preload=True, verbose=False)
 
-    def _load_single_cleaned_mne_object(self, subject_id):
+    def _load_single_cleaned_mne_object(self, subject_id, **_):
         # Create path
         path = os.path.join(self.get_mne_path(), "derivatives", subject_id, "eeg",
                             f"{subject_id}_task-eyesclosed_eeg.set")
