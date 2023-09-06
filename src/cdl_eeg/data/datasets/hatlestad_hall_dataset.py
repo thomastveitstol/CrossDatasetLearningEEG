110b2c70fbb6b665bdd889c19680ef1cf9b96497
thomas <thomas.tveitstol@gmail.com>
2023-09-06 14:33:48 +0200

    implemented channel system methods

--- src/cdl_eeg/data/datasets/hatlestad_hall_dataset.py
+++ src/cdl_eeg/data/datasets/hatlestad_hall_dataset.py
@@ -1,13 +1,10 @@
-import os.path
+import os
 
 import mne
 import numpy
+import pandas
 
-from cdl_eeg.data.datasets.dataset_base import ChannelSystemBase, EEGDatasetBase
-
-
-class HatlestadHallChannelSystem(ChannelSystemBase):
-    ...
+from cdl_eeg.data.datasets.dataset_base import EEGDatasetBase
 
 
 class HatlestadHall(EEGDatasetBase):
@@ -21,8 +18,11 @@ class HatlestadHall(EEGDatasetBase):
     __slots__ = ()
 
     def __init__(self):
-        super().__init__(channel_system=HatlestadHallChannelSystem())
+        super().__init__()
 
+    # ----------------
+    # Loading methods
+    # ----------------
     def _load_single_raw_mne_object(self, subject_id, **kwargs):
         # Extract session (should be 't1' or 't2'). 't2' is not available for all subjects
         session = kwargs["session"]
@@ -76,3 +76,54 @@ class HatlestadHall(EEGDatasetBase):
 
         # Load MNE object and return
         return mne.io.read_epochs_eeglab(input_fname=path, verbose=False)
+
+    # ----------------
+    # Channel system
+    # ----------------
+    def _get_template_electrode_positions(self):
+        """
+
+        Examples
+        -------
+        >>> HatlestadHall()._get_template_electrode_positions()["FT8"]
+        (85.7567, 27.8641, -3.1488)
+        """
+        # Create path
+        path = os.path.join(self.get_mne_path(), "code", "bidsify-srm-restingstate", "chanlocs",
+                            "BioSemi_SRM_template_64_locs.xyz")
+
+        # Make pandas dataframe
+        df = pandas.read_table(path, header=None, delim_whitespace=True).rename(columns={1: "x", 2: "y", 3: "z",
+                                                                                         4: "ch_name"})
+
+        # Extract the needed values
+        x_vals = df["x"].to_numpy()
+        y_vals = df["y"].to_numpy()
+        z_vals = df["z"].to_numpy()
+
+        ch_names = tuple(df["ch_name"])
+
+        # Convert to dict and return
+        return {ch_name: (x, y, z) for ch_name, x, y, z in zip(ch_names, x_vals, y_vals, z_vals)}
+
+    def channel_name_to_index(self):
+        """
+
+        Examples
+        -------
+        >>> HatlestadHall().channel_name_to_index()["FT8"]
+        42
+        """
+        # Create path
+        path = os.path.join(self.get_mne_path(), "code", "bidsify-srm-restingstate", "chanlocs",
+                            "BioSemi_SRM_template_64_locs.xyz")
+
+        # Make pandas dataframe
+        df = pandas.read_table(path, header=None, delim_whitespace=True).rename(columns={0: "idx", 4: "ch_name"})
+
+        # Extract the needed values
+        indices = df["idx"].to_numpy() - 1  # Need to subtract 1 due to python 0 zero indexing
+        ch_names = tuple(df["ch_name"])
+
+        # Convert to dict and return
+        return {ch_name: idx for ch_name, idx in zip(ch_names, indices)}
