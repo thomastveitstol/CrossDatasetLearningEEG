dae66a726b185e0c5f43ff0b40d633ece66af8fe
thomas <thomas.tveitstol@gmail.com>
2023-09-06 09:25:47 +0200

    made plot script

---
+++ scripts/visualisations/data/hatlestad_hall.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot
+
+from cdl_eeg.data.datasets.hatlestad_hall import HatlestadHall
+
+
+def main():
+    # -----------------
+    # Hyperparameters
+    # -----------------
+    # Data
+    subject_number = 9
+    derivatives = True
+    session = "t1"
+
+    # Pre-processing
+    filtering = None
+    resampling_freq = None
+    notch_filter = None
+    avg_reference = False
+
+    # -----------------
+    # Load data
+    # -----------------
+    subject_id = HatlestadHall().get_subject_ids()[subject_number]
+    eeg = HatlestadHall().load_single_mne_object(subject_id=subject_id, derivatives=derivatives, session=session)
+
+    # Pre-process
+    eeg = HatlestadHall.pre_process(eeg, filtering=filtering, notch_filter=notch_filter, avg_reference=avg_reference,
+                                    resample=resampling_freq)
+
+    # -----------------
+    # Plot data
+    # -----------------
+    eeg.plot()
+    eeg.compute_psd().plot()
+
+    pyplot.show()
+
+
+if __name__ == "__main__":
+    main()
