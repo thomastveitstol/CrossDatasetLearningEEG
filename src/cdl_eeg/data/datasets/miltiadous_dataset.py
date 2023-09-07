36488aa2f97087a454b72454083c5017c3532caa
thomas <thomas.tveitstol@gmail.com>
2023-09-07 10:47:53 +0200

    added methods

--- src/cdl_eeg/data/datasets/miltiadous_dataset.py
+++ src/cdl_eeg/data/datasets/miltiadous_dataset.py
@@ -15,6 +15,9 @@ class Miltiadous(EEGDatasetBase):
 
     __slots__ = ()
 
+    _channel_names = ("Fp1", "Fp2", "F3", "F4", "C3", "C4", "P3", "P4", "O1", "O2", "F7", "F8", "T3", "T4", "T5", "T6",
+                      "Fz", "Cz", "Pz")
+
     def __init__(self):
         super().__init__()
 
@@ -40,5 +43,14 @@ class Miltiadous(EEGDatasetBase):
     # ----------------
     # Methods for channel system
     # ----------------
+    def _get_template_electrode_positions(self):
+        # Following the international 10-20 system according to the README file. Thus using MNE default
+        montage = mne.channels.make_standard_montage("standard_1020")
+        channel_positions = montage.get_positions()["ch_pos"]
+
+        # Return dict with channel positions, keeping only the ones in the data
+        return {ch_name: tuple(pos) for ch_name, pos in channel_positions.items() if ch_name in self._channel_names}
+
     def channel_name_to_index(self):
-        raise NotImplementedError
+        # todo: make tests based on .tsv files, as I only checked a single subject for channel names and ordering
+        return {ch_name: i for i, ch_name in enumerate(self._channel_names)}
