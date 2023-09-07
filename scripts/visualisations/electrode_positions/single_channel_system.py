47a3b6a15f747f0e391cd1666d529c973bcef954
thomas <thomas.tveitstol@gmail.com>
2023-09-07 10:36:11 +0200

    added todo

---
+++ scripts/visualisations/electrode_positions/single_channel_system.py
@@ -0,0 +1,34 @@
+"""
+Plot script for plotting the electrode positions of a single channel system/dataset
+"""
+from matplotlib import pyplot
+
+from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
+from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
+from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
+
+
+def main():
+    # Select dataset
+    name = "Rockhill"
+
+    # Get dataset
+    available_datasets = (HatlestadHall(), Rockhill(), YulinWang, Miltiadous())
+    for available_dataset in available_datasets:
+        if name in (available_dataset.name, type(available_dataset).__name__):
+            dataset = available_dataset
+            break
+    else:
+        raise ValueError(f"The dataset {name} was not recognised")
+
+    # ----------------
+    # Plotting
+    # ----------------
+    dataset.plot_electrode_positions()
+
+    pyplot.show()
+
+
+if __name__ == "__main__":
+    main()
