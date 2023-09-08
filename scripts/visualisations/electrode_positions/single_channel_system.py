9c85193ea84a60e22936df3087b9b8f1c88b52ec
thomas <thomas.tveitstol@gmail.com>
2023-09-08 14:58:01 +0200

    added dataset to plot script

--- scripts/visualisations/electrode_positions/single_channel_system.py
+++ scripts/visualisations/electrode_positions/single_channel_system.py
@@ -5,17 +5,18 @@ from matplotlib import pyplot
 
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+from cdl_eeg.data.datasets.mpi_lemon import MPILemon
 from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
 from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
 
 def main():
     # Select dataset
-    name = "YulinWang"
+    name = "MPILemon"
     subject_id = None
 
     # Get dataset
-    available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous)
+    available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon)
     for available_dataset in available_datasets:
         if name in (available_dataset().name, available_dataset.__name__):
             dataset = available_dataset()
