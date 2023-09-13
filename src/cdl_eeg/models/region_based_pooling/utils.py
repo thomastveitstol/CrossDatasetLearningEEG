437b42ceaf5be9c65040ca4f5929adbc6d02b059
thomas <thomas.tveitstol@gmail.com>
2023-09-13 13:01:30 +0200

    added __len__ methods

--- src/cdl_eeg/models/region_based_pooling/utils.py
+++ src/cdl_eeg/models/region_based_pooling/utils.py
@@ -17,7 +17,17 @@ class ChannelsInRegion(NamedTuple):
     """
     ch_names: Tuple[str, ...]
 
+    def __len__(self) -> int:
+        """The length of a ChannelsInRegion() object should correspond to the number of channels in that region (given
+        the channel system). Following the notation from the Region Based Pooling paper (Tveitstøl et al., 2023,
+        submitted) this is |R^(i)_j cap C|"""
+        return len(self.ch_names)
+
 
 @dataclasses.dataclass
 class ChannelsInRegionSplit:
     ch_names: Dict[RegionID, ChannelsInRegion]  # As of Python version >= 3.7, dicts are ordered
+
+    def __len__(self) -> int:
+        """The length of a ChannelsInRegionSplit() object should correspond to the number of regions"""
+        return len(self.ch_names)
