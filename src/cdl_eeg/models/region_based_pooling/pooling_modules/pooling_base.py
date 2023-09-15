3cecb14c64042b50f10d74843044c6943cdde937
thomas <thomas.tveitstol@gmail.com>
2023-09-15 13:02:32 +0200

    made new base class

--- src/cdl_eeg/models/region_based_pooling/pooling_modules/pooling_base.py
+++ src/cdl_eeg/models/region_based_pooling/pooling_modules/pooling_base.py
@@ -3,28 +3,34 @@ import abc
 import torch.nn as nn
 
 
-class SingleRegionPoolingBase(nn.Module, abc.ABC):
+class PoolingModuleBase(nn.Module, abc.ABC):
+    """
+    Pooling module base
+    """
+
+
+class SingleRegionPoolingBase(PoolingModuleBase):
     """
     Base class for pooling modules operating on a single region. This offers great flexibility, but may come at a
     computational cost (for-loops are slow)
     """
 
 
-class MultiRegionPoolingBase(nn.Module, abc.ABC):
+class MultiRegionPoolingBase(PoolingModuleBase):
     """
     Base class for pooling modules operating on a subset of regions in a single channel split. If the idea is to operate
     on all regions in a region/channel split, inherit from SingleChannelSplitPoolingBase instead
     """
 
 
-class SingleChannelSplitPoolingBase(nn.Module, abc.ABC):
+class SingleChannelSplitPoolingBase(PoolingModuleBase):
     """
     Base class for pooling modules operating on an entire channel split. This may be preferred to single regions, as it
     allows for more time-efficient implementations
     """
 
 
-class MultiChannelSplitsPoolingBase(nn.Module, abc.ABC):
+class MultiChannelSplitsPoolingBase(PoolingModuleBase):
     """
     Base class for pooling modules operating on multiple channel splits. This may be preferred, as it allows for more
     time-efficient implementations
