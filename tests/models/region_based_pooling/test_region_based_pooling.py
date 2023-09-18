773ab01173172c7e3d324e07f6b0aa72432febb3
thomas <thomas.tveitstol@gmail.com>
2023-09-18 15:04:50 +0200

    made unit test

--- tests/models/region_based_pooling/test_region_based_pooling.py
+++ tests/models/region_based_pooling/test_region_based_pooling.py
@@ -1,4 +1,5 @@
 import mne
+import torch
 
 from cdl_eeg.data.datasets.dataset_base import ChannelSystem
 from cdl_eeg.models.region_based_pooling.region_based_pooling import SingleChannelRegionBasedPooling
@@ -78,3 +79,66 @@ def test_fit_channel_system():
     for expected_regions, ch_split in zip(num_regions, ch_splits):
         assert expected_regions == len(ch_split), (f"Expected number of regions to be {expected_regions}, but found "
                                                    f"{len(ch_split)}")
+
+
+def test_forward():
+    """Test the forward method. That it runs, and that output types and shapes are as expected"""
+    # ----------------
+    # Generate dummy channel system and data
+    # ----------------
+    # Hyperparameters
+    batch_size, time_steps = 10, 2_000
+    channel_system_name = "TestName"
+    x_min, x_max, y_min, y_max = -.17, .17, -.17, .17
+    montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
+
+    # Channel system requirements
+    electrode_positions = Electrodes3D(montage.get_positions()["ch_pos"])
+    channel_name_to_index = {name: i for i, name in enumerate(electrode_positions.positions)}
+    channel_system = ChannelSystem(name=channel_system_name, channel_name_to_index=channel_name_to_index,
+                                   electrode_positions=electrode_positions)
+
+    # Data
+    data = torch.rand(size=(batch_size, len(electrode_positions), time_steps))
+
+    # ----------------
+    # Make RBP object and fit channel system (tested above)
+    # ----------------
+    # Hyperparameters
+    num_regions = 11, 7, 26, 18
+
+    pooling_methods = ("SingleCSMean", "SingleCSMean", "SingleCSMean", "SingleCSMean")
+    pooling_methods_kwargs = ({}, {}, {}, {})
+    split_methods = ("VoronoiSplit", "VoronoiSplit", "VoronoiSplit", "VoronoiSplit")
+    box_params = {"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max}
+    split_methods_kwargs = ({"num_points": num_regions[0], **box_params}, {"num_points": num_regions[1], **box_params},
+                            {"num_points": num_regions[2], **box_params}, {"num_points": num_regions[3], **box_params})
+
+    # Make object
+    rbp_module = SingleChannelRegionBasedPooling(pooling_methods=pooling_methods,
+                                                 pooling_methods_kwargs=pooling_methods_kwargs,
+                                                 split_methods=split_methods,
+                                                 split_methods_kwargs=split_methods_kwargs)
+
+    # Fit channel system
+    rbp_module.fit_channel_system(channel_system=channel_system)
+
+    # ----------------
+    # Run forward method
+    # ----------------
+    outputs = rbp_module(data, channel_system_name=channel_system_name, channel_name_to_index=channel_name_to_index)
+
+    # ----------------
+    # Tests
+    # ----------------
+    # Type check
+    assert isinstance(outputs, tuple), f"Expected outputs to be a tuple, but found {type(outputs)}"
+
+    # Check that all elements are torch tensors
+    assert all(isinstance(out, torch.Tensor) for out in outputs)
+
+    # Check if the sizes are correct
+    assert all(out.size() == torch.Size([batch_size, expected_regions, time_steps])
+               for out, expected_regions in zip(outputs, num_regions))
+
+    # todo: check if any tensor is empty or not
