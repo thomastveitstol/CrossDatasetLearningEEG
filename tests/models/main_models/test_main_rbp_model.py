735c6a89341320bcd1e8d640bd1fd786d778f9b2
thomas <thomas.tveitstol@gmail.com>
2023-09-21 13:24:17 +0200

    fixed/updated unit test

--- tests/models/main_models/test_main_rbp_model.py
+++ tests/models/main_models/test_main_rbp_model.py
@@ -1,10 +1,9 @@
-import itertools
-
 import mne
 import torch
 
 from cdl_eeg.data.datasets.dataset_base import ChannelSystem
-from cdl_eeg.models.main_models.main_rbp_model import MainSingleChannelSplitRBPModel, MainMultiChannelSplitsRBPModel
+from cdl_eeg.models.main_models.main_rbp_model import MainRBPModel
+from cdl_eeg.models.region_based_pooling.region_based_pooling import RBPDesign, RBPPoolType
 from cdl_eeg.models.region_based_pooling.utils import Electrodes3D
 
 
@@ -29,106 +28,61 @@ def test_forward_single_channel_system():
     data = torch.rand(size=(batch_size, len(electrode_positions), time_steps))
 
     # ----------------
+    # Make RBP designs
+    # ----------------
+    box_params = {"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max}
+
+    # Design 1
+    num_regions_1 = (11, 7, 26, 4, 7)
+    num_channel_splits_1 = len(num_regions_1)
+
+    design_1 = RBPDesign(
+        pooling_type=RBPPoolType.MULTI_CS, pooling_methods="MultiCSSharedRocket",
+        pooling_methods_kwargs={"num_regions": num_regions_1, "num_kernels": 43, "max_receptive_field": 37},
+        split_methods=tuple("VoronoiSplit" for _ in range(num_channel_splits_1)),
+        split_methods_kwargs=tuple({"num_points": num_regs, **box_params} for num_regs in num_regions_1)
+    )
+
+    # Design 2
+    num_regions_2 = (9, 14)
+
+    design_2 = RBPDesign(
+        pooling_type=RBPPoolType.SINGLE_CS, pooling_methods=("SingleCSMean", "SingleCSSharedRocket"),
+        pooling_methods_kwargs=({}, {"num_regions": num_regions_2[1], "num_kernels": 93, "max_receptive_field": 67}),
+        split_methods=("VoronoiSplit", "VoronoiSplit"),
+        split_methods_kwargs=tuple({"num_points": num_regs, **box_params} for num_regs in num_regions_2)
+    )
+
+    # Design 3
+    num_regions_3 = (5, 5, 7, 5, 12, 10)
+    num_channel_splits_3 = len(num_regions_3)
+
+    design_3 = RBPDesign(
+        pooling_type=RBPPoolType.MULTI_CS, pooling_methods="MultiCSSharedRocket",
+        pooling_methods_kwargs={"num_regions": num_regions_3, "num_kernels": 5, "max_receptive_field": 55},
+        split_methods=tuple("VoronoiSplit" for _ in range(num_channel_splits_3)),
+        split_methods_kwargs=tuple({"num_points": num_regs, **box_params} for num_regs in num_regions_3)
+    )
+
+    # ----------------
     # Make model and fit channel system
     # ----------------
-    # Hyperparameters
-    num_regions = 11, 7, 26, 18
     num_classes = 5
+    num_regions = num_regions_1 + num_regions_2 + num_regions_3
 
     mts_module = "InceptionTime"
     mts_module_kwargs = {"in_channels": sum(num_regions), "num_classes": num_classes}
-    pooling_methods = ("SingleCSSharedRocket", "SingleCSMean", "SingleCSMean", "SingleCSSharedRocket")
-    pooling_methods_kwargs = ({"num_regions": num_regions[0], "num_kernels": 300, "max_receptive_field": 73},
-                              {},
-                              {},
-                              {"num_regions": num_regions[3], "num_kernels": 23, "max_receptive_field": 131})
-    split_methods = ("VoronoiSplit", "VoronoiSplit", "VoronoiSplit", "VoronoiSplit")
-    box_params = {"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max}
-    split_methods_kwargs = ({"num_points": num_regions[0], **box_params}, {"num_points": num_regions[1], **box_params},
-                            {"num_points": num_regions[2], **box_params}, {"num_points": num_regions[3], **box_params})
 
-    # Make model
-    model = MainSingleChannelSplitRBPModel(
-        mts_module=mts_module, mts_module_kwargs=mts_module_kwargs, pooling_methods=pooling_methods,
-        pooling_methods_kwargs=pooling_methods_kwargs, split_methods=split_methods,
-        split_methods_kwargs=split_methods_kwargs)
-
-    # Fit channel system
+    model = MainRBPModel(mts_module=mts_module, mts_module_kwargs=mts_module_kwargs,
+                         rbp_designs=(design_1, design_2, design_3))
     model.fit_channel_system(channel_system)
 
     # ----------------
     # Pre-compute and run forward method
     # ----------------
     pre_computed = model.pre_compute(data)
-    outputs = model(data, channel_system_name=channel_system_name, channel_name_to_index=channel_name_to_index,
-                    pre_computed=pre_computed)
-
-    # ----------------
-    # Tests
-    # ----------------
-    # Type check
-    assert isinstance(outputs, torch.Tensor), (f"Expected the output to be a torch.Tensor, but outputs had type "
-                                               f"{type(outputs)}")
-
-    # Shape check
-    expected_size = torch.Size([batch_size, num_classes])
-    assert outputs.size() == expected_size, f"Expected output to have shape {expected_size}, but found {outputs.size()}"
-
-
-def test_forward_multi_channel_system():
-    """Test forward method when only one channel system has been fit"""
-    # ----------------
-    # Generate dummy channel system and data
-    # ----------------
-    # Hyperparameters
-    batch_size, time_steps = 10, 2_000
-    channel_system_name = "TestName"
-    x_min, x_max, y_min, y_max = -.17, .17, -.17, .17
-    montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
-
-    # Channel system requirements
-    electrode_positions = Electrodes3D(montage.get_positions()["ch_pos"])
-    channel_name_to_index = {name: i for i, name in enumerate(electrode_positions.positions)}
-    channel_system = ChannelSystem(name=channel_system_name, channel_name_to_index=channel_name_to_index,
-                                   electrode_positions=electrode_positions)
-
-    # Data
-    data = torch.rand(size=(batch_size, len(electrode_positions), time_steps))
-
-    # ----------------
-    # Make model and fit channel system
-    # ----------------
-    # Hyperparameters
-    num_regions = ((11, 7, 26), (4, 7), (3, 4, 3, 6))
-    num_channel_splits = tuple(len(regions) for regions in num_regions)
-    num_classes = 5
-
-    mts_module = "InceptionTime"
-    mts_module_kwargs = {"in_channels": sum(tuple(itertools.chain(*num_regions))), "num_classes": num_classes}
-    pooling_methods = ("MultiCSSharedRocket", "MultiCSSharedRocket", "MultiCSSharedRocket")
-    pooling_methods_kwargs = ({"num_regions": num_regions[0], "num_kernels": 43, "max_receptive_field": 37},
-                              {"num_regions": num_regions[1], "num_kernels": 5, "max_receptive_field": 75},
-                              {"num_regions": num_regions[2], "num_kernels": 17, "max_receptive_field": 57})
-    split_methods = tuple(tuple("VoronoiSplit" for _ in range(num_regs)) for num_regs in num_channel_splits)
-    box_params = {"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max}
-    split_methods_kwargs = tuple(tuple({"num_points": num_regs, **box_params} for num_regs in regions)
-                                 for regions in num_regions)
-
-    # Make model
-    model = MainMultiChannelSplitsRBPModel(
-        mts_module=mts_module, mts_module_kwargs=mts_module_kwargs, pooling_methods=pooling_methods,
-        pooling_methods_kwargs=pooling_methods_kwargs, split_methods=split_methods,
-        split_methods_kwargs=split_methods_kwargs)
-
-    # Fit channel system
-    model.fit_channel_system(channel_system)
-
-    # ----------------
-    # Pre-compute and run forward method
-    # ----------------
-    pre_computed = model.pre_compute(data)
-    outputs = model(data, channel_system_name=channel_system_name, channel_name_to_index=channel_name_to_index,
-                    pre_computed=pre_computed)
+    outputs = model(data, channel_system_name=channel_system.name,
+                    channel_name_to_index=channel_system.channel_name_to_index, pre_computed=pre_computed)
 
     # ----------------
     # Tests
