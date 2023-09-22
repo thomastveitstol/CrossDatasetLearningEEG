7d27d62eaf35b51da28b3992cc742f61b7d9b844
thomas <thomas.tveitstol@gmail.com>
2023-09-22 09:49:08 +0200

    added transformation method in base class

--- src/cdl_eeg/models/transformations/base.py
+++ src/cdl_eeg/models/transformations/base.py
@@ -23,3 +23,27 @@ class TransformationBase(abc.ABC):
 
         # Convert to tuple and return
         return tuple(transformations)
+
+    def transform(self, method):
+        """
+        Method for getting the specified transformation method
+
+        Parameters
+        ----------
+        method : str
+            The selected transformation method. Must correspond to the name of an implemented method
+
+        Returns
+        -------
+        typing.Callable
+            See the individual transformation methods for more details on return type
+        """
+        # Input check
+        if method not in self.get_available_transformations():
+            raise ValueError(f"Transformation method '{method}' was not recognised. Make sure that the method passed "
+                             f"shares the name with the implemented method you want to use. The transformation methods "
+                             f"available for this class ({type(self).__name__}) are: "
+                             f"{self.get_available_transformations()}")
+
+        # Return the method itself
+        return getattr(self, method)
