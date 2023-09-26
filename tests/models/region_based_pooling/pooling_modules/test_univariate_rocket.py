b4199f12a0cb05af36b9a36b5297646dafeae20b
thomas <thomas.tveitstol@gmail.com>
2023-09-26 13:40:12 +0200

    updated unit test

--- tests/models/region_based_pooling/pooling_modules/test_univariate_rocket.py
+++ tests/models/region_based_pooling/pooling_modules/test_univariate_rocket.py
@@ -1,7 +1,9 @@
+from typing import Dict, Tuple
+
 import torch
 
-from cdl_eeg.models.region_based_pooling.pooling_modules.univariate_rocket import SingleCSSharedRocket, \
-    MultiCSSharedRocket
+from cdl_eeg.models.region_based_pooling.pooling_modules.univariate_rocket import MultiCSSharedRocket, \
+    SingleCSSharedRocket
 from cdl_eeg.models.region_based_pooling.utils import ChannelsInRegionSplit, RegionID, ChannelsInRegion
 
 
@@ -69,7 +71,10 @@ def test_multi_cs_shared_rocket_forward():
     # ---------------
     # Prepare inputs to forward method
     # ---------------
-    # Create dummy data  todo: improve dummy data usage
+    channel_splits: Dict[str, Tuple[ChannelsInRegionSplit, ...]] = dict()
+    channel_name_to_index: Dict[str, Dict[str, int]] = dict()
+
+    # First dataset  todo: improve dummy data usage
     channel_split_0 = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("E", "C", "B")),
                                              RegionID(1): ChannelsInRegion(("E", "F", "A")),
                                              RegionID(2): ChannelsInRegion(("F", "A", "B", "C")),
@@ -84,22 +89,44 @@ def test_multi_cs_shared_rocket_forward():
     channel_split_2 = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("B", "F")),
                                              RegionID(1): ChannelsInRegion(("B", "A", "C")),
                                              RegionID(2): ChannelsInRegion(("A",))})
-    channel_splits = (channel_split_0, channel_split_1, channel_split_2)
 
-    channel_name_to_index = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
+    channel_splits["d1"] = (channel_split_0, channel_split_1, channel_split_2)
+    channel_name_to_index["d1"] = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
 
-    batch_size, time_steps = 10, 2000
-    x = torch.rand(batch_size, len(channel_name_to_index), time_steps)
+    # Second dataset
+    channel_split_0 = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("q", "w", "e")),
+                                             RegionID(1): ChannelsInRegion(("r",)),
+                                             RegionID(2): ChannelsInRegion(("t", "r", "e", "b")),
+                                             RegionID(3): ChannelsInRegion(("h", "j"))})
+    channel_split_1 = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("c", "t", "q", "b")),
+                                             RegionID(1): ChannelsInRegion(("q", "c")),
+                                             RegionID(2): ChannelsInRegion(("s",)),
+                                             RegionID(3): ChannelsInRegion(("h",)),
+                                             RegionID(4): ChannelsInRegion(("j", "e", "r", "k", "j", "w", "q")),
+                                             RegionID(5): ChannelsInRegion(("e", "r", "b", "a")),
+                                             RegionID(6): ChannelsInRegion(("s", "q", "w"))})
+    channel_split_2 = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("e",)),
+                                             RegionID(1): ChannelsInRegion(("r",)),
+                                             RegionID(2): ChannelsInRegion(("l",))})
+
+    channel_splits["d2"] = (channel_split_0, channel_split_1, channel_split_2)
+    channel_name_to_index["d2"] = {"q": 0, "w": 1, "e": 2, "r": 3, "t": 4, "b": 5, "h": 6, "j": 7, "c": 8, "s": 9,
+                                   "k": 10, "l": 11, "a": 12}
 
     # ---------------
     # Pre-compute and run forward method
     # ---------------
-    pre_computed = model.pre_compute(x)
-    outputs = model(x, pre_computed=pre_computed, channel_splits=channel_splits,
+    time_steps = 2000
+    input_tensors = {"d1": torch.rand(size=(13, len(channel_name_to_index["d1"]), time_steps)),
+                     "d2": torch.rand(size=(31, len(channel_name_to_index["d2"]), time_steps))}
+
+    pre_computed = model.pre_compute(input_tensors)
+    outputs = model(input_tensors, pre_computed=pre_computed, channel_splits=channel_splits,
                     channel_name_to_index=channel_name_to_index)
 
     # ---------------
     # Tests
+    # todo: test if the batches will be stacked as expected
     # todo: test that the matrix multiplication is safe and as expected
     # ---------------
     # Type check
@@ -109,5 +136,6 @@ def test_multi_cs_shared_rocket_forward():
     assert all(isinstance(out, torch.Tensor) for out in outputs)
 
     # Check if the sizes are correct
-    assert all(out.size() == torch.Size([batch_size, expected_regions, time_steps])
+    expected_batch_size = 31 + 13
+    assert all(out.size() == torch.Size([expected_batch_size, expected_regions, time_steps])
                for out, expected_regions in zip(outputs, num_regions))
