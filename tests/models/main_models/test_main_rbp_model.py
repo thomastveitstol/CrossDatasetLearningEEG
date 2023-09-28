2a2f82d8588e0770ebcbd4f80082f2789e0a5a51
thomas <thomas.tveitstol@gmail.com>
2023-09-28 14:18:58 +0200

    fixed test

--- tests/models/main_models/test_main_rbp_model.py
+++ tests/models/main_models/test_main_rbp_model.py
@@ -24,34 +24,46 @@ from cdl_eeg.models.transformations.frequency_slowing import FrequencySlowing
 from cdl_eeg.models.transformations.utils import UnivariateUniform
 
 
-@pytest.mark.skip(reason="The test must be updated for the newest implementations")
-def test_forward_single_channel_system():
+def test_forward():
     """Test forward method when only one channel system has been fit"""
     # ----------------
-    # Generate dummy channel system and data
+    # Generate dummy channel system
     # ----------------
-    # Hyperparameters
-    batch_size, time_steps = 10, 2_000
-    channel_system_name = "TestName"
-    x_min, x_max, y_min, y_max = -.17, .17, -.17, .17
-    montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
+    # Channel system 1
+    montage_1 = mne.channels.make_standard_montage("GSN-HydroCel-129")
+    electrode_positions_1 = Electrodes3D(montage_1.get_positions()["ch_pos"])
+    ch_name_to_idx_1 = {name: i for i, name in enumerate(electrode_positions_1.positions)}
+    channel_system_1 = ChannelSystem(name="TestName1", channel_name_to_index=ch_name_to_idx_1,
+                                     electrode_positions=electrode_positions_1)
 
-    # Channel system requirements
-    electrode_positions = Electrodes3D(montage.get_positions()["ch_pos"])
-    channel_name_to_index = {name: i for i, name in enumerate(electrode_positions.positions)}
-    channel_system = ChannelSystem(name=channel_system_name, channel_name_to_index=channel_name_to_index,
-                                   electrode_positions=electrode_positions)
+    # Channel system 2
+    montage_2 = mne.channels.make_standard_montage("biosemi64")
+    electrode_positions_2 = Electrodes3D(montage_2.get_positions()["ch_pos"])
+    ch_name_to_idx_2 = {name: i for i, name in enumerate(electrode_positions_2.positions)}
+    channel_system_2 = ChannelSystem(name="TestName2", channel_name_to_index=ch_name_to_idx_2,
+                                     electrode_positions=electrode_positions_2)
+
+    # All channel systems
+    channel_systems = (channel_system_1, channel_system_2)
+    channel_name_to_index = {"TestName1": ch_name_to_idx_1, "TestName2": ch_name_to_idx_2}
 
     # Data
-    data = torch.rand(size=(batch_size, len(electrode_positions), time_steps))
+    time_steps = 2_113
+    batch_size_1, batch_size_2 = 31, 13
+
+    data_1 = torch.rand(size=(batch_size_1, len(electrode_positions_1), time_steps))
+    data_2 = torch.rand(size=(batch_size_2, len(electrode_positions_1), time_steps))
+
+    data = {"TestName1": data_1, "TestName2": data_2}
 
     # ----------------
     # Make RBP designs
     # ----------------
+    x_min, x_max, y_min, y_max = -.17, .17, -.17, .17
     box_params = {"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max}
 
     # Design 1
-    num_regions_1 = (11, 7, 26, 4, 7)
+    num_regions_1 = (5, 5, 5)
     num_channel_splits_1 = len(num_regions_1)
 
     design_1 = RBPDesign(
@@ -62,17 +74,19 @@ def test_forward_single_channel_system():
     )
 
     # Design 2
-    num_regions_2 = (9, 14)
+    num_regions_2 = 3
+    num_designs_2 = 7
 
     design_2 = RBPDesign(
-        pooling_type=RBPPoolType.SINGLE_CS, pooling_methods=("SingleCSMean", "SingleCSSharedRocket"),
-        pooling_methods_kwargs=({}, {"num_regions": num_regions_2[1], "num_kernels": 93, "max_receptive_field": 67}),
-        split_methods=("VoronoiSplit", "VoronoiSplit"),
-        split_methods_kwargs=tuple({"num_points": num_regs, **box_params} for num_regs in num_regions_2)
+        pooling_type=RBPPoolType.SINGLE_CS, pooling_methods="SingleCSSharedRocket",
+        pooling_methods_kwargs={"num_regions": num_regions_2, "num_kernels": 42, "max_receptive_field": 67},
+        split_methods="VoronoiSplit",
+        split_methods_kwargs={"num_points": num_regions_2, **box_params},
+        num_designs=num_designs_2
     )
 
     # Design 3
-    num_regions_3 = (5, 5, 7, 5, 12, 10)
+    num_regions_3 = (5, 5, 3, 5, 6, 3)
     num_channel_splits_3 = len(num_regions_3)
 
     design_3 = RBPDesign(
@@ -82,25 +96,36 @@ def test_forward_single_channel_system():
         split_methods_kwargs=tuple({"num_points": num_regs, **box_params} for num_regs in num_regions_3)
     )
 
+    # Design 4
+    num_regions_4 = 4
+    num_designs_4 = 5
+
+    design_4 = RBPDesign(
+        pooling_type=RBPPoolType.SINGLE_CS, pooling_methods="SingleCSMean",
+        pooling_methods_kwargs={},
+        split_methods="VoronoiSplit",
+        split_methods_kwargs={"num_points": num_regions_4, **box_params},
+        num_designs=num_designs_4
+    )
+
     # ----------------
     # Make model and fit channel system
     # ----------------
     num_classes = 5
-    num_regions = num_regions_1 + num_regions_2 + num_regions_3
+    num_regions = num_regions_1 + (num_regions_2,) * num_designs_2 + num_regions_3 + (num_regions_4,) * num_designs_4
 
     mts_module = "InceptionTime"
     mts_module_kwargs = {"in_channels": sum(num_regions), "num_classes": num_classes}
 
     model = MainRBPModel(mts_module=mts_module, mts_module_kwargs=mts_module_kwargs,
-                         rbp_designs=(design_1, design_2, design_3))
-    model.fit_channel_system(channel_system)
+                         rbp_designs=(design_1, design_2, design_3, design_4))
+    model.fit_channel_systems(channel_systems)
 
     # ----------------
     # Pre-compute and run forward method
     # ----------------
     pre_computed = model.pre_compute(data)
-    outputs = model(data, channel_system_name=channel_system.name,
-                    channel_name_to_index=channel_system.channel_name_to_index, pre_computed=pre_computed)
+    outputs = model(data, channel_name_to_index=channel_name_to_index, pre_computed=pre_computed)
 
     # ----------------
     # Tests
@@ -110,7 +135,7 @@ def test_forward_single_channel_system():
                                                f"{type(outputs)}")
 
     # Shape check
-    expected_size = torch.Size([batch_size, num_classes])
+    expected_size = torch.Size([(batch_size_1 + batch_size_2), num_classes])
     assert outputs.size() == expected_size, f"Expected output to have shape {expected_size}, but found {outputs.size()}"
 
 
