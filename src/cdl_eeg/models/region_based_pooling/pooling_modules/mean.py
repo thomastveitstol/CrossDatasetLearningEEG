7a906b8acfce26688615fa03327163c276bce26e
thomas <thomas.tveitstol@gmail.com>
2023-09-28 11:34:58 +0200

    fixed pooling class for heterogeneous batch requirements

--- src/cdl_eeg/models/region_based_pooling/pooling_modules/mean.py
+++ src/cdl_eeg/models/region_based_pooling/pooling_modules/mean.py
@@ -16,7 +16,7 @@ class SingleCSMean(SingleChannelSplitPoolingBase):
     """
 
     @staticmethod
-    def forward(x, *, channel_split, channel_name_to_index):
+    def forward(input_tensors, *, channel_splits, channel_name_to_index):
         """
         Forward method
 
@@ -25,12 +25,12 @@ class SingleCSMean(SingleChannelSplitPoolingBase):
 
         Parameters
         ----------
-        x : torch.Tensor
-            A tensor containing EEG data with shape=(batch, channels, time_steps). Note that the channels are correctly
-            selected within this method, and the EEG data should be the full data matrix (such that
-            channel_name_to_index maps correctly)
-        channel_split : cdl_eeg.models.region_based_pooling.utils.ChannelsInRegionSplit
-        channel_name_to_index : dict[str, int]
+        input_tensors : dict[str, torch.Tensor]
+            A dict with keys being dataset names and values are tensors containing EEG data with
+            shape=(batch, channels, time_steps). Note that the channels are correctly selected within this method, and
+            the EEG data should be the full data matrix (such that channel_name_to_index maps correctly)
+        channel_splits : dict[str, cdl_eeg.models.region_based_pooling.utils.ChannelsInRegionSplit]
+        channel_name_to_index : dict[str, dict[str, int]]
 
         Returns
         -------
@@ -39,26 +39,49 @@ class SingleCSMean(SingleChannelSplitPoolingBase):
         Examples
         --------
         >>> from cdl_eeg.models.region_based_pooling.utils import ChannelsInRegionSplit, RegionID, ChannelsInRegion
-        >>> my_channels = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("A", "C", "E")),
-        ...                                      RegionID(1): ChannelsInRegion(("C", "E", "B", "A"))})
-        >>> name_2_idx = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
+        >>> my_channels_a = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("A", "C", "E")),
+        ...                                        RegionID(1): ChannelsInRegion(("C", "E", "B", "A"))})
+        >>> my_channels_b = ChannelsInRegionSplit({RegionID(0): ChannelsInRegion(("E1", "E3", "E2")),
+        ...                                        RegionID(1): ChannelsInRegion(("E2",))})
+        >>> name_2_idx = {"a": {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}, "b": {"E1": 0, "E2": 1, "E3": 2}}
         >>> my_model = SingleCSMean()
-        >>> my_model(torch.rand(size=(10, 6, 1000)), channel_split=my_channels, channel_name_to_index=name_2_idx).size()
-        torch.Size([10, 2, 1000])
+        >>> my_model({"a": torch.rand(size=(10, 6, 1000)), "b": torch.rand(size=(7, 3, 1000))},
+        ...          channel_splits={"a": my_channels_a, "b": my_channels_b}, channel_name_to_index=name_2_idx).size()
+        torch.Size([17, 2, 1000])
         """
-        # Initialise tensor which will contain all region representations
-        batch, _, time_steps = x.size()
-        num_regions = len(channel_split)
-        region_representations = torch.empty(size=(batch, num_regions, time_steps)).to(x.device)
+        # TODO: I think this may be move to base class?
+        # Loop through all datasets
+        dataset_region_representations = []
+        for dataset_name, x in input_tensors.items():
+            # (I take no chances on all input being ordered similarly)
+            dataset_ch_name_to_idx = channel_name_to_index[dataset_name]
+            ch_splits = channel_splits[dataset_name]
 
-        # Loop through all regions (dicts are ordered by default, meaning the ordering of the tensor will follow the
-        # ordering of the dict keys)
-        for i, channels in enumerate(channel_split.ch_names.values()):
-            # Extract the indices of the legal channels for this region
-            allowed_node_indices = channel_names_to_indices(ch_names=channels.ch_names,
-                                                            channel_name_to_index=channel_name_to_index)
+            # Perform forward pass
+            dataset_region_representations.append(
+                _forward_single_dataset(x, channel_split=ch_splits, channel_name_to_index=dataset_ch_name_to_idx))
 
-            # Compute region representation by averaging and insert it
-            region_representations[:, i] = torch.mean(x[:, allowed_node_indices])  # Consider to keep dim in the future
+        # Concatenate the data together
+        return torch.cat(dataset_region_representations, dim=0)
 
-        return region_representations
+
+# -----------------
+# Functions
+# -----------------
+def _forward_single_dataset(x, *, channel_split, channel_name_to_index):
+    # Initialise tensor which will contain all region representations
+    batch, _, time_steps = x.size()
+    num_regions = len(channel_split)
+    region_representations = torch.empty(size=(batch, num_regions, time_steps)).to(x.device)
+
+    # Loop through all regions (dicts are ordered by default, meaning the ordering of the tensor will follow the
+    # ordering of the dict keys)
+    for i, channels in enumerate(channel_split.ch_names.values()):
+        # Extract the indices of the legal channels for this region
+        allowed_node_indices = channel_names_to_indices(ch_names=channels.ch_names,
+                                                        channel_name_to_index=channel_name_to_index)
+
+        # Compute region representation by averaging and insert it
+        region_representations[:, i] = torch.mean(x[:, allowed_node_indices])  # Consider to keep dim in the future
+
+    return region_representations
