f537610eb966803efc774d06a05678490e0cd8ea
thomas <thomas.tveitstol@gmail.com>
2023-09-28 14:05:31 +0200

    fixed test

--- tests/models/region_based_pooling/test_region_based_pooling.py
+++ tests/models/region_based_pooling/test_region_based_pooling.py
@@ -1,5 +1,4 @@
 import mne
-import pytest
 import torch
 
 from cdl_eeg.data.datasets.dataset_base import ChannelSystem
@@ -95,72 +94,74 @@ def test_single_cs_fit_channel_system():
             f"Expected the channel '{ch_name}' to be contained in a region, but found no match"
 
 
-# TODO: not updated
-@pytest.mark.skip(reason="The test must be updated for the newest implementations")
 def test_single_cs_forward():
     """Test the forward method. That it runs, and that output types and shapes are as expected"""
     # ----------------
     # Generate dummy channel system and data
     # ----------------
-    # Hyperparameters
-    batch_size, time_steps = 10, 2_000
-    channel_system_name = "TestName"
     x_min, x_max, y_min, y_max = -.17, .17, -.17, .17
-    montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
 
-    # Channel system requirements
-    electrode_positions = Electrodes3D(montage.get_positions()["ch_pos"])
-    channel_name_to_index = {name: i for i, name in enumerate(electrode_positions.positions)}
-    channel_system = ChannelSystem(name=channel_system_name, channel_name_to_index=channel_name_to_index,
-                                   electrode_positions=electrode_positions)
+    # Channel system 1
+    montage_1 = mne.channels.make_standard_montage("GSN-HydroCel-129")
+    electrode_positions_1 = Electrodes3D(montage_1.get_positions()["ch_pos"])
+    ch_name_to_idx_1 = {name: i for i, name in enumerate(electrode_positions_1.positions)}
+    channel_system_1 = ChannelSystem(name="TestName1", channel_name_to_index=ch_name_to_idx_1,
+                                     electrode_positions=electrode_positions_1)
+
+    # Channel system 2
+    montage_2 = mne.channels.make_standard_montage("biosemi64")
+    electrode_positions_2 = Electrodes3D(montage_2.get_positions()["ch_pos"])
+    ch_name_to_idx_2 = {name: i for i, name in enumerate(electrode_positions_2.positions)}
+    channel_system_2 = ChannelSystem(name="TestName2", channel_name_to_index=ch_name_to_idx_2,
+                                     electrode_positions=electrode_positions_2)
+
+    # All channel systems
+    channel_systems = (channel_system_1, channel_system_2)
+    channel_name_to_index = {"TestName1": ch_name_to_idx_1, "TestName2": ch_name_to_idx_2}
 
     # Data
-    data = torch.rand(size=(batch_size, len(electrode_positions), time_steps))
+    time_steps = 2_000
+    batch_size_1, batch_size_2 = 31, 13
+
+    data_1 = torch.rand(size=(batch_size_1, len(electrode_positions_1), time_steps))
+    data_2 = torch.rand(size=(batch_size_2, len(electrode_positions_1), time_steps))
+
+    data = {"TestName1": data_1, "TestName2": data_2}
 
     # ----------------
     # Make RBP object and fit channel system (tested above)
     # ----------------
-    # Hyperparameters
-    num_regions = 11, 7, 26, 18
+    num_regions = 5
 
-    pooling_methods = ("SingleCSSharedRocket", "SingleCSMean", "SingleCSMean", "SingleCSSharedRocket")
-    pooling_methods_kwargs = ({"num_regions": num_regions[0], "num_kernels": 300, "max_receptive_field": 73},
-                              {},
-                              {},
-                              {"num_regions": num_regions[3], "num_kernels": 23, "max_receptive_field": 131})
-    split_methods = ("VoronoiSplit", "VoronoiSplit", "VoronoiSplit", "VoronoiSplit")
+    pooling_method = "SingleCSSharedRocket"
+    pooling_method_kwargs = {"num_regions": num_regions, "num_kernels": 43, "max_receptive_field": 37}
+    split_method = "VoronoiSplit"
     box_params = {"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max}
-    split_methods_kwargs = ({"num_points": num_regions[0], **box_params}, {"num_points": num_regions[1], **box_params},
-                            {"num_points": num_regions[2], **box_params}, {"num_points": num_regions[3], **box_params})
+    split_method_kwargs = {"num_points": num_regions, **box_params}
 
-    # Make object
-    rbp_module = SingleChannelSplitRegionBasedPooling(pooling_methods=pooling_methods,
-                                                      pooling_methods_kwargs=pooling_methods_kwargs,
-                                                      split_methods=split_methods,
-                                                      split_methods_kwargs=split_methods_kwargs)
+    rbp_module = SingleChannelSplitRegionBasedPooling(pooling_method=pooling_method,
+                                                      pooling_method_kwargs=pooling_method_kwargs,
+                                                      split_method=split_method,
+                                                      split_method_kwargs=split_method_kwargs)
 
     # Fit channel system
-    rbp_module.fit_channel_system(channel_system=channel_system)
+    rbp_module.fit_channel_systems(channel_systems=channel_systems)
 
     # ----------------
     # Pre-compute and run forward method
     # ----------------
     pre_computed = rbp_module.pre_compute(data)
-    outputs = rbp_module(data, channel_system_name=channel_system_name, channel_name_to_index=channel_name_to_index,
-                         pre_computed=pre_computed)
+    outputs = rbp_module(data, channel_name_to_index=channel_name_to_index, pre_computed=pre_computed)
 
     # ----------------
     # Tests
     # ----------------
     # Type check
-    assert isinstance(outputs, tuple), f"Expected outputs to be a tuple, but found {type(outputs)}"
+    assert isinstance(outputs, torch.Tensor), f"Expected outputs to be a torch.Tensor, but found {type(outputs)}"
 
-    # Check that all elements are torch tensors
-    assert all(isinstance(out, torch.Tensor) for out in outputs)
-
-    # Check if the sizes are correct
-    assert all(out.size() == torch.Size([batch_size, expected_regions, time_steps])
-               for out, expected_regions in zip(outputs, num_regions))
+    # Check if the size is correct
+    expected_batch_size = batch_size_1 + batch_size_2
+    assert outputs.size() == torch.Size([expected_batch_size, num_regions, time_steps])
 
     # todo: check if any tensor is empty or not
 
