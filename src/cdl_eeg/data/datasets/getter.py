f057df03596c7546e4671d13877954f62b0fce0f
thomas <thomas.tveitstol@gmail.com>
2023-09-29 15:32:04 +0200

    added getter function for datasets

---
+++ src/cdl_eeg/data/datasets/getter.py
@@ -0,0 +1,30 @@
+from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
+
+
+def get_dataset(dataset_name, **kwargs):
+    """
+    Function for getting the specified dataset
+
+    Parameters
+    ----------
+    dataset_name : str
+        Dataset name
+    kwargs
+        Key word arguments
+
+    Returns
+    -------
+    cdl_eeg.data.datasets.dataset_base.EEGDatasetBase
+    """
+    # All available datasets must be included here
+    available_datasets = (Miltiadous, Rockhill)
+
+    # Loop through and select the correct one
+    for dataset in available_datasets:
+        if dataset_name in (dataset.__name__, dataset().name):
+            return dataset(**kwargs)
+
+    # If no match, an error is raised
+    raise ValueError(f"The dataset '{dataset_name}' was not recognised. Please select among the following: "
+                     f"{tuple(dataset.__name__ for dataset in available_datasets)}")
