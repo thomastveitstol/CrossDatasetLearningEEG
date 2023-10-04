d5cd130ef0ada912bb1be29649b59ad90812385b
thomas <thomas.tveitstol@gmail.com>
2023-10-04 12:45:50 +0200

    t-SNE script improvements

--- scripts/models/training/tsne_debug_script.py
+++ scripts/models/training/tsne_debug_script.py
@@ -7,20 +7,21 @@ what does not)
 import os
 import warnings
 
+import numpy
+from matplotlib import pyplot
 import torch
 import torch.nn as nn
 from torch import optim
 import yaml
 from torch.utils.data import DataLoader
 
-from cdl_eeg.data.combined_datasets import LoadDetails, CombinedDatasets
+from cdl_eeg.data.combined_datasets import CombinedDatasets
 from cdl_eeg.data.data_generators.data_generator import SelfSupervisedDataGenerator
-from cdl_eeg.data.data_split import KFoldDataSplit
-from cdl_eeg.data.datasets.getter import get_dataset
+from cdl_eeg.data.data_split import KFoldDataSplit, leave_1_fold_out
 from cdl_eeg.models.main_models.main_rbp_model import MainRBPModel, tensor_dict_to_device
 from cdl_eeg.models.region_based_pooling.region_based_pooling import RBPDesign, RBPPoolType
 from cdl_eeg.models.transformations.frequency_slowing import FrequencySlowing
-from cdl_eeg.models.transformations.utils import UnivariateUniform
+from cdl_eeg.models.transformations.utils import get_random_distribution
 
 
 def main():
@@ -72,52 +73,42 @@ def main():
                              rbp_designs=tuple(rbp_designs)).to(device)
 
     # -----------------
-    # Load data
+    # Load data for SSL pre-training
     # -----------------
-    load_details = []
-    datasets = []
-    subjects = dict()
-    channel_name_to_index = dict()
-    for dataset_name, dataset_details in config["Datasets"].items():
-        # Get dataset
-        dataset = get_dataset(dataset_name)
-        datasets.append(dataset)
-        dataset_subjects = dataset.get_subject_ids()[:dataset_details["num_subjects"]]
-        subjects[dataset_name] = dataset_subjects
-        channel_name_to_index[dataset_name] = dataset.channel_name_to_index()
+    # Load all data (no targets to load, as SSL)
+    combined_dataset = CombinedDatasets.from_config(config=config["SSL Datasets"], target=None)
 
-        # Construct loading details
-        load_details.append(
-            LoadDetails(subject_ids=dataset_subjects, time_series_start=dataset_details["time_series_start"],
-                        num_time_steps=dataset_details["num_time_steps"])
-        )
-
-    # Load all data
-    combined_dataset = CombinedDatasets(tuple(datasets), load_details=tuple(load_details))
+    # Extract some details of the datasets (needed later)
+    subjects = combined_dataset.dataset_subjects
+    datasets = combined_dataset.datasets
+    channel_name_to_index = {dataset.name: dataset.channel_name_to_index() for dataset in datasets}
 
     # -----------------
     # Split data
     # -----------------
     # Split the data
-    conf_data_split = config["Data Split"]
+    conf_data_split = config["SSL Data Split"]
     data_split = KFoldDataSplit(num_folds=conf_data_split["num_folds"], dataset_subjects=subjects,
                                 seed=conf_data_split["seed"]).folds
 
     # Split into training and validation splits
-    train_subjects = data_split[0]  # todo
+    train_subjects = leave_1_fold_out(i=-1, folds=data_split)
     val_subjects = data_split[-1]
 
     # -----------------
     # Define pretext task
     # -----------------
     config_pretext = config["Pretext Task"]
-    slowing_distribution = UnivariateUniform(lower=config_pretext["lower"], upper=config_pretext["upper"])
+    slowing_distribution = get_random_distribution(distribution=config_pretext["Distribution"]["distribution"],
+                                                   **config_pretext["Distribution"]["kwargs"])
     transformation = FrequencySlowing(slowing_distribution=slowing_distribution)
     pretext_task = config_pretext["task"]
 
     # -----------------
     # Perform pre-training
     # -----------------
+    print("======================================")
+    print("============ Pre-Training ============")
     # Fit channel systems
     model.fit_channel_systems(tuple(data.channel_system for data in datasets))
 
@@ -159,30 +150,23 @@ def main():
                     optimiser=optimiser, num_epochs=config_training["num_epochs"], verbose=config_training["verbose"],
                     channel_name_to_index=channel_name_to_index, device=device)
 
+    print("========= Pre-Training Done ==========")
+    print("======================================")
+
     # -----------------
     # t-SNE
     # -----------------
-    load_details = []  # TODO: reloading
-    datasets = []
-    subjects = dict()
-    channel_name_to_index = dict()
-    for dataset_name, dataset_details in config["t-SNE Data"].items():
-        # Get dataset
-        dataset = get_dataset(dataset_name)
-        datasets.append(dataset)
-        dataset_subjects = dataset.get_subject_ids()[:dataset_details["num_subjects"]]
-        subjects[dataset_name] = dataset_subjects
-        channel_name_to_index[dataset_name] = dataset.channel_name_to_index()
-
-        # Construct loading details
-        load_details.append(
-            LoadDetails(subject_ids=dataset_subjects, time_series_start=dataset_details["time_series_start"],
-                        num_time_steps=dataset_details["num_time_steps"])
-        )
-
+    print("\n\n")
+    print("=====================================")
+    print("=============== t-SNE ===============")
     # Load all data
-    combined_dataset = CombinedDatasets(tuple(datasets), load_details=tuple(load_details),
-                                        target=config["t-SNE Details"]["target"])
+    combined_dataset = CombinedDatasets.from_config(config=config["t-SNE Data"],
+                                                    target=config["t-SNE Details"]["target"])
+
+    # Extract some details of the datasets (needed later)
+    subjects = combined_dataset.dataset_subjects
+    datasets = combined_dataset.datasets
+    channel_name_to_index = {dataset.name: dataset.channel_name_to_index() for dataset in datasets}
 
     # 'Split' the data  todo
     data_split = KFoldDataSplit(num_folds=1, dataset_subjects=subjects, seed=config["t-SNE Details"]["seed"]).folds
@@ -206,19 +190,20 @@ def main():
                                channel_name_to_index=channel_name_to_index,
                                pre_computed=pre_computed)
 
-    # todo
-    import numpy
+    # todo: hard-coded for healthy/parkinsons
     healthy = tsne_data[numpy.where(targets["rockhill"] < 0.5)].T
     parkinsons = tsne_data[numpy.where(targets["rockhill"] > 0.5)].T
 
     print("Plotting t-SNE...")
-    print(tsne_data.shape)
-    from matplotlib import pyplot
     pyplot.scatter(healthy[0], healthy[1], marker='o', s=50)
     pyplot.scatter(parkinsons[0], parkinsons[1], marker='o', s=50)
+
     pyplot.title('t-SNE Plot')
     pyplot.xlabel('Dimension 1')
     pyplot.ylabel('Dimension 2')
+
+    print("============ t-SNE done! ============")
+    print("=====================================")
 
     pyplot.show()
 
