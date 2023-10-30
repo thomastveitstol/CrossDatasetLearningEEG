b793b09b45011e60fe5f9066b707ac9c912ffd28
thomas <thomas.tveitstol@gmail.com>
2023-10-30 12:38:15 +0100

    added dataset to plot script

--- scripts/visualisations/electrode_positions/projected_channel_systems.py
+++ scripts/visualisations/electrode_positions/projected_channel_systems.py
@@ -7,17 +7,18 @@ from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
 from cdl_eeg.data.datasets.mpi_lemon import MPILemon
 from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
+from cdl_eeg.data.datasets.van_hees import VanHees
 from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
 
 def main():
     # Select dataset
-    names = "HatlestadHall", "YulinWang", "Rockhill", "Miltiadous"
+    names = "HatlestadHall", "YulinWang", "Rockhill", "Miltiadous", "VanHees"
 
     # Loop through all names
     for name in names:
         # Get dataset
-        available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon)
+        available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon, VanHees)
         for available_dataset in available_datasets:
             if name in (available_dataset().name, available_dataset.__name__):
                 dataset = available_dataset()
