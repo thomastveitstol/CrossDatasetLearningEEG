5487930bdf85c8b7bd897e66b3542498daf52f45
thomas <thomas.tveitstol@gmail.com>
2023-10-30 13:46:26 +0100

    added dataset to getter function

--- src/cdl_eeg/data/datasets/getter.py
+++ src/cdl_eeg/data/datasets/getter.py
@@ -1,6 +1,7 @@
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
 from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
+from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
 
 def get_dataset(dataset_name, **kwargs):
@@ -19,7 +20,7 @@ def get_dataset(dataset_name, **kwargs):
     cdl_eeg.data.datasets.dataset_base.EEGDatasetBase
     """
     # All available datasets must be included here
-    available_datasets = (Miltiadous, Rockhill, HatlestadHall)
+    available_datasets = (Miltiadous, Rockhill, HatlestadHall, YulinWang)
 
     # Loop through and select the correct one
     for dataset in available_datasets:
