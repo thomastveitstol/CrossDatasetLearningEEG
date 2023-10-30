55cb07f28a1abe46c71892fe6f6ab3e44aa31f3a
thomas <thomas.tveitstol@gmail.com>
2023-10-30 10:41:44 +0100

    made script for plotting 2D projections

---
+++ scripts/visualisations/electrode_positions/projected_channel_systems.py
@@ -0,0 +1,37 @@
+"""
+Script for plotting 2D projected channel systems
+"""
+from matplotlib import pyplot
+
+from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
+from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+from cdl_eeg.data.datasets.mpi_lemon import MPILemon
+from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
+from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
+
+
+def main():
+    # Select dataset
+    names = "HatlestadHall", "Rockhill", "Miltiadous"
+
+    # Loop through all names
+    for name in names:
+        # Get dataset
+        available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon)
+        for available_dataset in available_datasets:
+            if name in (available_dataset().name, available_dataset.__name__):
+                dataset = available_dataset()
+                break
+        else:
+            raise ValueError(f"The dataset {name} was not recognised")
+
+        # ----------------
+        # Plotting
+        # ----------------
+        dataset.plot_2d_electrode_positions()
+
+    pyplot.show()
+
+
+if __name__ == "__main__":
+    main()
