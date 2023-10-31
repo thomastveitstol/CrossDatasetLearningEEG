d302c9f3078171f29728152a10fc76a3228cf2d6
thomas <thomas.tveitstol@gmail.com>
2023-10-31 10:10:12 +0100

    added OUS dataset to getter and yml file

--- src/cdl_eeg/data/datasets/getter.py
+++ src/cdl_eeg/data/datasets/getter.py
@@ -3,6 +3,7 @@ from typing import Type, Tuple
 from cdl_eeg.data.datasets.dataset_base import EEGDatasetBase
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+from cdl_eeg.data.datasets.ous_dataset import OUS
 from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
 from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
@@ -23,7 +24,7 @@ def get_dataset(dataset_name, **kwargs):
     EEGDatasetBase
     """
     # All available datasets must be included here
-    available_datasets: Tuple[Type[EEGDatasetBase], ...] = (Miltiadous, Rockhill, HatlestadHall, YulinWang)
+    available_datasets: Tuple[Type[EEGDatasetBase], ...] = (Miltiadous, Rockhill, HatlestadHall, YulinWang, OUS)
 
     # Loop through and select the correct one
     for dataset in available_datasets:
