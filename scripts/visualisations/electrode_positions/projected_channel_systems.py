557958d43a2f8b951b1e5a5aa5fe54e0c789e7a7
thomas <thomas.tveitstol@gmail.com>
2023-12-06 10:10:07 +0100

    added OUS channel system to plot script

--- scripts/visualisations/electrode_positions/projected_channel_systems.py
+++ scripts/visualisations/electrode_positions/projected_channel_systems.py
@@ -7,6 +7,7 @@ from cdl_eeg.data.datasets.cau_eeg_dataset import CAUEEG
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
 from cdl_eeg.data.datasets.mpi_lemon import MPILemon
+from cdl_eeg.data.datasets.ous_dataset import OUS
 from cdl_eeg.data.datasets.rockhill_dataset import Rockhill
 from cdl_eeg.data.datasets.van_hees import VanHees
 from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
@@ -19,7 +20,7 @@ def main():
     # Loop through all names
     for name in names:
         # Get dataset
-        available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon, VanHees, CAUEEG)
+        available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon, VanHees, CAUEEG, OUS)
         for available_dataset in available_datasets:
             if name in (available_dataset().name, available_dataset.__name__):
                 dataset = available_dataset()
