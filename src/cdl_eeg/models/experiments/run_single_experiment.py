021035d8ddf4e909e75b41c440cee8fa8cbdab34
thomas <thomas.tveitstol@gmail.com>
2024-01-17 10:37:46 +0100

    fixed test generator

--- src/cdl_eeg/models/experiments/run_single_experiment.py
+++ src/cdl_eeg/models/experiments/run_single_experiment.py
@@ -151,7 +151,7 @@ def run_experiment(config, results_path):
             test_pre_computed = model.pre_compute(
                 input_tensors={dataset_name: torch.tensor(data, dtype=torch.float).to(device)
                                for dataset_name, data in test_data.items()})
-            test_gen = DownstreamDataGenerator(data=val_data, targets=test_targets, pre_computed=test_pre_computed,
+            test_gen = DownstreamDataGenerator(data=test_data, targets=test_targets, pre_computed=test_pre_computed,
                                                subjects=combined_dataset.get_subjects_dict(test_subjects))
             test_loader = DataLoader(dataset=test_gen, batch_size=train_config["batch_size"], shuffle=True)
         else:
