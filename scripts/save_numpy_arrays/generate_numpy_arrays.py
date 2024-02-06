ced91e8d6853951b9b35a470f5155d3c6ea3581e
thomas <thomas.tveitstol@gmail.com>
2024-02-06 12:21:18 +0100

    added printing in numpy array saving script

--- scripts/save_numpy_arrays/generate_numpy_arrays.py
+++ scripts/save_numpy_arrays/generate_numpy_arrays.py
@@ -1,7 +1,7 @@
 """
 Script for generating numpy arrays, which pre-processing hyperparameters are sampled from a config file
 """
-import os.path
+import os
 from datetime import date, datetime
 
 import yaml
@@ -12,6 +12,8 @@ from cdl_eeg.data.paths import get_numpy_data_storage_path
 
 
 def main():
+    verbose = True
+
     # --------------
     # Load domains of design choices/hyperparameters (.yml file)
     # --------------
@@ -36,7 +38,13 @@ def main():
     # --------------
     # Perform pre-processing
     # --------------
-    for dataset_name, preprocessing_kwargs in config["datasets"].items():
+    num_datasets = len(config["datasets"])
+    for i, (dataset_name, preprocessing_kwargs) in config["datasets"].items():
+        # Maybe print the dataset we will preprocess and save
+        if verbose:
+            print(f"Preprocessing and saving dataset {i + 1}/{num_datasets}: '{dataset_name}'...")
+
+        # Save the data with the preprocessing specifications
         get_dataset(dataset_name).save_eeg_as_numpy_arrays(path=path, **preprocessing_kwargs, **config["general"])
 
 
