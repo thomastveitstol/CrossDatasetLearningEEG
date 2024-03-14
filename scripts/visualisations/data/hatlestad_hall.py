d905c08d2e1bb9d822ce36b8e8765be6e286209c
thomas <thomas.tveitstol@gmail.com>
2024-03-14 11:03:22 +0100

    fix in plot script

--- scripts/visualisations/data/hatlestad_hall.py
+++ scripts/visualisations/data/hatlestad_hall.py
@@ -25,8 +25,8 @@ def main():
     eeg = HatlestadHall().load_single_mne_object(subject_id=subject_id, derivatives=derivatives, session=session)
 
     # Pre-process
-    eeg = HatlestadHall.pre_process(eeg, filtering=filtering, notch_filter=notch_filter, avg_reference=avg_reference,
-                                    resample=resampling_freq)
+    eeg = HatlestadHall().pre_process(eeg, filtering=filtering, notch_filter=notch_filter, avg_reference=avg_reference,
+                                      resample=resampling_freq, remove_above_std=None, excluded_channels=None)
 
     # -----------------
     # Plot data
