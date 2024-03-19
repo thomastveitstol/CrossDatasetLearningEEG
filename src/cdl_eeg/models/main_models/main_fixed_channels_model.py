3d4b22dd618737c9f971c1ba90ae37fa0b098108
thomas <thomas.tveitstol@gmail.com>
2024-03-19 16:37:26 +0100

    added doctest and updated for use of domain discriminator

--- src/cdl_eeg/models/main_models/main_fixed_channels_model.py
+++ src/cdl_eeg/models/main_models/main_fixed_channels_model.py
@@ -4,6 +4,7 @@ import enlighten
 import torch
 import torch.nn as nn
 
+from cdl_eeg.models.domain_adaptation.domain_discriminators.getter import get_domain_discriminator
 from cdl_eeg.models.metrics import Histories
 from cdl_eeg.models.mts_modules.getter import get_mts_module
 
@@ -11,19 +12,105 @@ from cdl_eeg.models.mts_modules.getter import get_mts_module
 class MainFixedChannelsModel(nn.Module):
     """
     Main model when the number of input channels is fixed
+
+    Examples
+    --------
+    >>> my_mts_kwargs = {"in_channels": 19, "num_classes": 7, "depth": 3}
+    >>> my_dd_kwargs = {"hidden_units": (8, 4), "num_classes": 3}
+    >>> MainFixedChannelsModel("InceptionNetwork", mts_module_kwargs=my_mts_kwargs, domain_discriminator="FCModule",
+    ...                        domain_discriminator_kwargs=my_dd_kwargs)
+    MainFixedChannelsModel(
+      (_mts_module): InceptionNetwork(
+        (_inception_modules): ModuleList(
+          (0): _InceptionModule(
+            (_input_conv): Conv1d(19, 32, kernel_size=(1,), stride=(1,), padding=same, bias=False)
+            (_conv_list): ModuleList(
+              (0): Conv1d(32, 32, kernel_size=(40,), stride=(1,), padding=same, bias=False)
+              (1): Conv1d(32, 32, kernel_size=(20,), stride=(1,), padding=same, bias=False)
+              (2): Conv1d(32, 32, kernel_size=(10,), stride=(1,), padding=same, bias=False)
+            )
+            (_max_pool): MaxPool1d(kernel_size=3, stride=1, padding=1, dilation=1, ceil_mode=False)
+            (_conv_after_max_pool): Conv1d(19, 32, kernel_size=(1,), stride=(1,), padding=same, bias=False)
+            (_batch_norm): BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+          )
+          (1-2): 2 x _InceptionModule(
+            (_input_conv): Conv1d(128, 32, kernel_size=(1,), stride=(1,), padding=same, bias=False)
+            (_conv_list): ModuleList(
+              (0): Conv1d(32, 32, kernel_size=(40,), stride=(1,), padding=same, bias=False)
+              (1): Conv1d(32, 32, kernel_size=(20,), stride=(1,), padding=same, bias=False)
+              (2): Conv1d(32, 32, kernel_size=(10,), stride=(1,), padding=same, bias=False)
+            )
+            (_max_pool): MaxPool1d(kernel_size=3, stride=1, padding=1, dilation=1, ceil_mode=False)
+            (_conv_after_max_pool): Conv1d(128, 32, kernel_size=(1,), stride=(1,), padding=same, bias=False)
+            (_batch_norm): BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+          )
+        )
+        (_shortcut_layers): ModuleList(
+          (0): _ShortcutLayer(
+            (_conv): Conv1d(19, 128, kernel_size=(1,), stride=(1,), padding=same)
+            (_batch_norm): BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+          )
+        )
+        (_fc_layer): Linear(in_features=128, out_features=7, bias=True)
+      )
+      (_domain_discriminator): FCModule(
+        (_model): ModuleList(
+          (0): Linear(in_features=128, out_features=8, bias=True)
+          (1): Linear(in_features=8, out_features=4, bias=True)
+          (2): Linear(in_features=4, out_features=3, bias=True)
+        )
+      )
+    )
     """
 
-    def __init__(self, mts_module, **kwargs):
+    def __init__(self, mts_module, mts_module_kwargs, domain_discriminator, domain_discriminator_kwargs):
+        """
+        Initialise
+
+        Parameters
+        ----------
+        mts_module : str
+        mts_module_kwargs : dict[str, typing.Any]
+        domain_discriminator : str, optional
+        domain_discriminator_kwargs: dict[str, typing.Any] | None
+        """
         super().__init__()
 
         # ----------------
         # Create MTS module
         # ----------------
-        self._mts_module = get_mts_module(mts_module_name=mts_module, **kwargs)
+        self._mts_module = get_mts_module(mts_module_name=mts_module, **mts_module_kwargs)
+
+        # ----------------
+        # (Maybe) create domain discriminator
+        # ----------------
+        if domain_discriminator is None:
+            self._domain_discriminator = None
+        else:
+            # Set kwargs to empty dict if none are passed
+            domain_discriminator_kwargs = dict() if domain_discriminator_kwargs is None else domain_discriminator_kwargs
+
+            # Need to get input features from MTS module
+            domain_discriminator_kwargs["in_features"] = self._mts_module.latent_features_dim
+
+            self._domain_discriminator = get_domain_discriminator(
+                name=domain_discriminator, **domain_discriminator_kwargs
+            )
 
     @classmethod
-    def from_config(cls, config):
-        return cls(mts_module=config["name"], **config["kwargs"])
+    def from_config(cls, mts_config, discriminator_config):
+        """
+        Initialise from config file
+
+        Parameters
+        ----------
+        mts_config : dict[str, typing.Any]
+        discriminator_config : dict[str, typing.Any] | None
+        """
+        return cls(mts_module=mts_config["name"],
+                   mts_module_kwargs=mts_config["kwargs"],
+                   domain_discriminator=None if discriminator_config is None else discriminator_config["name"],
+                   domain_discriminator_kwargs=None if discriminator_config is None else discriminator_config["kwargs"])
 
     # ---------------
     # Methods for forward propagation
@@ -43,11 +130,12 @@ class MainFixedChannelsModel(nn.Module):
 
         Examples
         --------
-        >>> my_model = MainFixedChannelsModel("InceptionTime", in_channels=23, num_classes=11)
+        >>> my_mts_kwargs = {"in_channels": 23, "num_classes": 11, "depth": 3}
+        >>> my_dd_kwargs = {"hidden_units": (8, 4), "num_classes": 3}
+        >>> my_model = MainFixedChannelsModel("InceptionNetwork", mts_module_kwargs=my_mts_kwargs,
+        ...                                   domain_discriminator="FCModule", domain_discriminator_kwargs=my_dd_kwargs)
         >>> my_model(torch.rand(size=(10, 23, 300))).size()
         torch.Size([10, 11])
-        >>> my_model({"d1": torch.rand(size=(10, 23, 300)), "d2": torch.rand(size=(4, 23, 300))}).size()
-        torch.Size([14, 11])
         """
         # (Maybe) concatenate all tensors. This should be possible, as this class should ony be used with a fixed number
         # of input channels. However, such usage is not recommended, as it is not necessary to store in a dict. Storing
