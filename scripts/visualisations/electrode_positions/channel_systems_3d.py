85e8eb0bd9c0680fdfc4da87a6b3f3c14b017f41
thomas <thomas.tveitstol@gmail.com>
2024-03-22 09:58:55 +0100

    renamed file and improved plot script

--- scripts/visualisations/electrode_positions/channel_systems_3d.py
+++ scripts/visualisations/electrode_positions/channel_systems_3d.py
@@ -1,8 +1,11 @@
 """
 Plot script for plotting the electrode positions of a single channel system/dataset
 """
+from typing import List
+
 from matplotlib import pyplot
 
+from cdl_eeg.data.datasets.dataset_base import EEGDatasetBase
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
 from cdl_eeg.data.datasets.mpi_lemon import MPILemon
@@ -12,22 +15,27 @@ from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
 def main():
     # Select dataset
-    name = "MPILemon"
-    subject_id = "sub-032402"
+    names = "MPILemon", "HatlestadHall"
 
-    # Get dataset
+    # Get datasets
+    datasets: List[EEGDatasetBase] = []
     available_datasets = (HatlestadHall, Rockhill, YulinWang, Miltiadous, MPILemon)
-    for available_dataset in available_datasets:
-        if name in (available_dataset().name, available_dataset.__name__):
-            dataset = available_dataset()
-            break
-    else:
-        raise ValueError(f"The dataset {name} was not recognised")
+    for name in names:
+        for available_dataset in available_datasets:
+            if name in (available_dataset().name, available_dataset.__name__):
+                datasets.append(available_dataset())
+                break
+        else:
+            raise ValueError(f"The dataset {name} was not recognised")
 
     # ----------------
     # Plotting
     # ----------------
-    dataset.plot_electrode_positions(subject_id)
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for dataset in datasets:
+        dataset.plot_electrode_positions(ax=ax)
 
     pyplot.show()
 
