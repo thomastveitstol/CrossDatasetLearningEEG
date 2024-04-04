eae4fcca1ea2b9ef838eb3af9675e4ac987892fd
thomas <thomas.tveitstol@gmail.com>
2024-04-04 19:41:09 +0200

    minor configuration

--- scripts/save_numpy_arrays/generate_numpy_arrays.py
+++ scripts/save_numpy_arrays/generate_numpy_arrays.py
@@ -101,7 +101,7 @@ def main():
         get_dataset(dataset_name).save_epochs_as_numpy_arrays(
             path=path, frequency_bands=config["frequency_bands"],
             resample_fmax_multiples=config["resample_fmax_multiples"], **preprocessing_kwargs, **config["general"],
-            plot_data=True
+            plot_data=False
         )
 
 
