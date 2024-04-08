2272be20ad5779f7e7053fad961a9984bb6f6bcc
thomas <thomas.tveitstol@gmail.com>
2024-04-08 12:40:41 +0200

    fixes for saving config file

--- scripts/models/training/single_experiment.py
+++ scripts/models/training/single_experiment.py
@@ -15,6 +15,15 @@ from cdl_eeg.models.random_search.generate_config_file import generate_config_fi
 from cdl_eeg.models.random_search.run_single_experiment import Experiment
 
 
+def _str_to_bool(s):
+    if s == "True":
+        return True
+    elif s == "False":
+        return False
+    else:
+        raise ValueError(f"Unexpected string: {s}")
+
+
 def main():
     # ---------------
     # Load domains of design choices/hyperparameters (.yml file)
@@ -47,23 +56,36 @@ def main():
     # Select data pre-processing version
     # ---------------
     # Make selection. We will use the same for all datasets
-    available_versions = os.listdir(get_numpy_data_storage_path())
-    available_versions = tuple(version for version in available_versions if version[:5] == "debug")  # todo
-    pre_processed_version = random.choice(available_versions)
+    # todo: Not sure how I feel about this hard-coding
+    preprocessed_folder = "preprocessed_2024-04-05_114614"
+    available_versions = os.listdir(os.path.join(get_numpy_data_storage_path(), preprocessed_folder))
+    available_versions = tuple(version for version in available_versions if version[:5] == "data_")
+    pre_processed_folder = random.choice(available_versions)
+    pre_processed_version = os.path.join(preprocessed_folder, pre_processed_folder)
 
     # Add selection to the datasets
     datasets = tuple(config["Datasets"])  # Maybe this is not needed, but it feels safe
     for dataset in datasets:
         config["Datasets"][dataset]["pre_processed_version"] = pre_processed_version
 
-    # Add the preprocessing config file to results folder
-    shutil.copy(src=os.path.join(get_numpy_data_storage_path(), pre_processed_version, "preprocessing_config.yml"),
-                dst=results_path)
-
-    # Extract it
-    print(os.path.join(results_path, "preprocessing_config.yml"))
-    with open(os.path.join(results_path, "preprocessing_config.yml"), "r") as file:
+    # Extract preprocessing config
+    with open(os.path.join(get_numpy_data_storage_path(), preprocessed_folder, "config.yml"), "r") as file:
         pre_processed_config = yaml.safe_load(file)
+
+    # Add details
+    filtering = pre_processed_folder.split("_")[3].split(sep="-")
+    l_freq, h_freq = float(filtering[0]), float(filtering[1])
+    pre_processed_config["general"]["filtering"] = [l_freq, h_freq]
+    pre_processed_config["general"]["autoreject"] = _str_to_bool(pre_processed_folder.split("_")[5])
+    s_freq = float(pre_processed_folder.split("_")[-1]) * h_freq
+    pre_processed_config["general"]["resample"] = float(pre_processed_folder.split("_")[-1]) * h_freq
+    pre_processed_config["general"]["num_time_steps"] = int(s_freq * pre_processed_config["general"]["epoch_duration"])
+    del pre_processed_config["frequency_bands"]
+    del pre_processed_config["resample_fmax_multiples"]
+
+    # Save it
+    with open(os.path.join(results_path, "preprocessing_config.yml"), "w") as file:
+        yaml.safe_dump(pre_processed_config, file)
 
     # ---------------
     # Run experiments
