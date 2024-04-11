376c21f2faa4685c4531eb89ebdd6e3813d0ba27
thomas <thomas.tveitstol@gmail.com>
2024-04-11 11:01:36 +0200

    pragmatic fix of passing num_regions to mean pooling method

--- src/cdl_eeg/models/region_based_pooling/pooling_modules/mean.py
+++ src/cdl_eeg/models/region_based_pooling/pooling_modules/mean.py
@@ -20,8 +20,15 @@ class MultiMSMean(MultiMontageSplitsPoolingBase):
     False
     """
 
-    @staticmethod
-    def forward(input_tensors, *, channel_splits, channel_name_to_index):
+    def __init__(self, num_regions):
+        """Overriding the __init__ method is actually not needed, but still convenient for two reasons: (1) we can make
+        input checks during the forward pass, and (2) other code often 'blindly' pass 'num_regions' to the __init__
+        method, so now I don't need to change that code (which is working fine) to avoid errors"""
+        super().__init__()
+
+        self._num_regions = num_regions
+
+    def forward(self, input_tensors, *, channel_splits, channel_name_to_index):
         """
         Forward method
 
@@ -50,37 +57,36 @@ class MultiMSMean(MultiMontageSplitsPoolingBase):
 
             # Perform forward pass
             dataset_region_representations.append(
-                _forward_single_dataset(x, channel_splits=ch_splits, channel_name_to_index=dataset_ch_name_to_idx))
+                self._forward_single_dataset(x, channel_splits=ch_splits, channel_name_to_index=dataset_ch_name_to_idx))
 
         # Concatenate the data together
         return tuple(torch.cat(tensor, dim=0) for tensor in list(zip(*dataset_region_representations)))
 
+    def _forward_single_dataset(self, x, *, channel_splits, channel_name_to_index):
+        # --------------
+        # Loop through all channel/region splits
+        # --------------
+        output_channel_splits: List[torch.Tensor] = []
+        for channel_split, expected_num_regions in zip(channel_splits, self._num_regions):
+            # Initialise tensor which will contain all region representations
+            batch, _, time_steps = x.size()
+            num_regions = len(channel_split)
+            if num_regions != expected_num_regions:
+                raise RuntimeError(f"Expected {expected_num_regions} number of regions, but received {num_regions}")
+            region_representations = torch.empty(size=(batch, num_regions, time_steps)).to(x.device)
 
-# -----------------
-# Functions
-# -----------------
-def _forward_single_dataset(x, *, channel_splits, channel_name_to_index):
-    # --------------
-    # Loop through all channel/region splits
-    # --------------
-    output_channel_splits: List[torch.Tensor] = []
-    for channel_split in channel_splits:
-        # Initialise tensor which will contain all region representations
-        batch, _, time_steps = x.size()
-        num_regions = len(channel_split)
-        region_representations = torch.empty(size=(batch, num_regions, time_steps)).to(x.device)
+            # Loop through all regions (dicts are ordered by default, meaning the ordering of the tensor will follow the
+            # ordering of the dict keys)
+            for i, channels in enumerate(channel_split.values()):
+                # Extract the indices of the legal channels for this region
+                allowed_node_indices = channel_names_to_indices(ch_names=channels,
+                                                                channel_name_to_index=channel_name_to_index)
 
-        # Loop through all regions (dicts are ordered by default, meaning the ordering of the tensor will follow the
-        # ordering of the dict keys)
-        for i, channels in enumerate(channel_split.values()):
-            # Extract the indices of the legal channels for this region
-            allowed_node_indices = channel_names_to_indices(ch_names=channels,
-                                                            channel_name_to_index=channel_name_to_index)
+                # Compute region representation by averaging and insert it
+                region_representations[:, i] = torch.mean(x[:, allowed_node_indices])  # Consider to keep dim in the
+                # future
 
-            # Compute region representation by averaging and insert it
-            region_representations[:, i] = torch.mean(x[:, allowed_node_indices])  # Consider to keep dim in the future
+            # Append as montage split output
+            output_channel_splits.append(region_representations)
 
-        # Append as montage split output
-        output_channel_splits.append(region_representations)
-
-    return output_channel_splits
+        return output_channel_splits
