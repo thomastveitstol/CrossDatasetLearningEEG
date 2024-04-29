5bc3dee784f2f0740ac2557c573963c50187188b
thomas <thomas.tveitstol@gmail.com>
2024-04-29 10:48:38 +0200

    made unit test

--- tests/models/main_models/conftest.py
+++ tests/models/main_models/conftest.py
@@ -1,4 +1,14 @@
+import numpy.random
 import pytest
+import torch
+
+from cdl_eeg.data.datasets.cau_eeg_dataset import CAUEEG
+from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
+from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
+from cdl_eeg.data.datasets.mpi_lemon import MPILemon
+from cdl_eeg.data.datasets.td_brain import TDBrain
+from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
+from cdl_eeg.models.main_models.main_rbp_model import MainRBPModel
 
 
 @pytest.fixture
@@ -68,3 +78,49 @@ def rbp_model_configs():
     mts_config = {"model": "InceptionNetwork", "kwargs": {"depth": 12, "cnn_units": 31, "num_classes": 1}}
 
     return rbp_config, mts_config
+
+
+@pytest.fixture
+def all_datasets():
+    return Miltiadous(), CAUEEG(), HatlestadHall(), TDBrain(), YulinWang(), MPILemon()
+
+
+@pytest.fixture
+def fitted_rbp_model(rbp_model_configs, all_datasets, dummy_data):
+    # ------------------
+    # Create real channel systems
+    # ------------------
+    # Get their channel systems
+    channel_systems = {dataset.name: dataset.channel_system for dataset in all_datasets}
+
+    # ----------------
+    # Make model supporting RBP
+    # ----------------
+    # Define model
+    rbp_config, mts_config = rbp_model_configs
+    model = MainRBPModel.from_config(rbp_config=rbp_config, mts_config=mts_config, discriminator_config=None)
+
+    # Fit channel systems
+    model.fit_channel_systems(tuple(channel_systems.values()))
+
+    # Fit CMMN layer
+    if model.any_rbp_cmmn_layers:
+        model.fit_psd_barycenters(data=dummy_data, channel_systems=channel_systems, sampling_freq=128)
+        model.fit_monge_filters(data=dummy_data, channel_systems=channel_systems)
+
+    # Return it
+    return model
+
+
+@pytest.fixture
+def dummy_data(all_datasets):
+    # Some configurations
+    num_time_steps = 500
+    batch_sizes = tuple(numpy.random.randint(10, 31) for _ in all_datasets)
+
+    # Create random tensors
+    data = {}
+    for batch_size, dataset in zip(batch_sizes, all_datasets):
+        data[dataset.name] = torch.rand(size=(batch_size, dataset.num_channels, num_time_steps))
+
+    return data
--- tests/models/main_models/test_main_rbp_model.py
+++ tests/models/main_models/test_main_rbp_model.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+import torch
 
 from cdl_eeg.data.datasets.cau_eeg_dataset import CAUEEG
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
@@ -40,3 +41,27 @@ def test_fit_real_channel_systems(rbp_model_configs):
                    for rbp_module in model._region_based_pooling._rbp_modules), \
             (f"Expected all channel systems to be fit to all RBP modules, but this was not the case for the channel "
              f"system {channel_system.name}")
+
+
+@pytest.mark.skipif(not os.path.isdir(get_raw_data_storage_path()), reason="Required datasets not available")
+def test_forward_shape(fitted_rbp_model, all_datasets, dummy_data, rbp_model_configs):
+    # Get the channel names to indices
+    channel_name_to_index = {dataset.name: dataset.channel_system.channel_name_to_index for dataset in all_datasets}
+
+    # ----------------
+    # Pre-compute and run forward method
+    # ----------------
+    pre_computed = fitted_rbp_model.pre_compute(dummy_data)
+    outputs = fitted_rbp_model(dummy_data, channel_name_to_index=channel_name_to_index, pre_computed=pre_computed)
+
+    # ----------------
+    # Tests
+    # ----------------
+    # Type check
+    assert isinstance(outputs, torch.Tensor), (f"Expected the output to be a torch.Tensor, but outputs had type "
+                                               f"{type(outputs)}")
+
+    # Shape check
+    batch_size = sum(data.size()[0] for data in dummy_data.values())
+    expected_size = torch.Size((batch_size, rbp_model_configs[1]["kwargs"]["num_classes"]))
+    assert outputs.size() == expected_size, f"Expected output to have shape {expected_size}, but found {outputs.size()}"
