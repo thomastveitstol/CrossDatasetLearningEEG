946f23bd7b6360b8208b5302947c7bfa1b4e1849
thomas <thomas.tveitstol@gmail.com>
2024-05-28 09:57:40 +0200

    docstrings and imports

--- scripts/visualisations/electrode_positions/channel_systems_3d.py
+++ scripts/visualisations/electrode_positions/channel_systems_3d.py
@@ -1,5 +1,6 @@
 """
-Plot script for plotting the electrode positions of a single channel system/dataset
+Plot script for plotting the electrode positions of channel systems. Just for convenience really, as it is nice to
+visually check that it is as expected
 """
 from typing import List
 
@@ -9,16 +10,17 @@ from cdl_eeg.data.datasets.dataset_base import EEGDatasetBase
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
 from cdl_eeg.data.datasets.mpi_lemon import MPILemon
+from cdl_eeg.data.datasets.td_brain import TDBrain
 from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
 
 def main():
-    # Select dataset
-    names = "MPILemon", "HatlestadHall"
+    # Select datasets to plot
+    names = "MPILemon", "HatlestadHall", "TDBrain"
 
     # Get datasets
     datasets: List[EEGDatasetBase] = []
-    available_datasets = (HatlestadHall, YulinWang, Miltiadous, MPILemon)
+    available_datasets = (HatlestadHall, YulinWang, Miltiadous, MPILemon, TDBrain)
     for name in names:
         for available_dataset in available_datasets:
             if name in (available_dataset().name, available_dataset.__name__):
--- scripts/visualisations/electrode_positions/projected_channel_systems.py
+++ scripts/visualisations/electrode_positions/projected_channel_systems.py
@@ -3,22 +3,21 @@ Script for plotting 2D projected channel systems
 """
 from matplotlib import pyplot
 
-from cdl_eeg.data.datasets.cau_eeg_dataset import CAUEEG
 from cdl_eeg.data.datasets.hatlestad_hall_dataset import HatlestadHall
 from cdl_eeg.data.datasets.miltiadous_dataset import Miltiadous
 from cdl_eeg.data.datasets.mpi_lemon import MPILemon
-from cdl_eeg.data.datasets.ous_dataset import OUS
+from cdl_eeg.data.datasets.td_brain import TDBrain
 from cdl_eeg.data.datasets.yulin_wang_dataset import YulinWang
 
 
 def main():
     # Select dataset
-    names = "HatlestadHall", "YulinWang", "Miltiadous", "CAUEEG"
+    names = "HatlestadHall", "YulinWang", "Miltiadous"
 
     # Loop through all names
     for name in names:
         # Get dataset
-        available_datasets = (HatlestadHall, YulinWang, Miltiadous, MPILemon, CAUEEG, OUS)
+        available_datasets = (HatlestadHall, YulinWang, Miltiadous, MPILemon,  TDBrain)
         for available_dataset in available_datasets:
             if name in (available_dataset().name, available_dataset.__name__):
                 dataset = available_dataset()
