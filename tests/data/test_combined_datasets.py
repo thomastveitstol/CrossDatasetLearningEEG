d62357df410e00a0e80e8b9754094c728d729d80
thomas <thomas.tveitstol@gmail.com>
2024-05-29 09:40:56 +0200

    fixed tests

--- tests/data/test_combined_datasets.py
+++ tests/data/test_combined_datasets.py
@@ -12,24 +12,26 @@ from cdl_eeg.data.paths import get_numpy_data_storage_path
 
 
 @pytest.mark.skipif(not os.path.isdir(get_numpy_data_storage_path()), reason="Required numpy arrays not available")
-def test_data_loading():
+@pytest.mark.parametrize("required_target", [None, "age", "sex"])
+def test_data_loading(required_target):
     """Test that the __init__ method runs"""
     # ------------------
     # Define the datasets to test
     # ------------------
     # Selecting datasets
-    datasets = (Miltiadous(), HatlestadHall())
+    datasets = (MPILemon(), Miltiadous(), HatlestadHall())
 
     # Defining load details
-    details_1 = LoadDetails(subject_ids=MPILemon().get_subject_ids()[:9], num_time_steps=1_003)
-    details_2 = LoadDetails(subject_ids=Miltiadous().get_subject_ids()[:14], num_time_steps=305)
-    details_3 = LoadDetails(subject_ids=HatlestadHall().get_subject_ids()[:3], time_series_start=1_000,
-                            num_time_steps=975)
+    version = "preprocessed_2024-05-13_173548/data_band_pass_8-12_autoreject_True_sampling_multiple_4"
+    details_1 = LoadDetails(subject_ids=MPILemon().get_subject_ids()[:9], pre_processed_version=version)
+    details_2 = LoadDetails(subject_ids=Miltiadous().get_subject_ids()[:14], pre_processed_version=version)
+    details_3 = LoadDetails(subject_ids=HatlestadHall().get_subject_ids()[:3], pre_processed_version=version)
 
     # ------------------
     # Load all data
     # ------------------
-    combined_dataset = CombinedDatasets(datasets, load_details=(details_1, details_2, details_3))
+    combined_dataset = CombinedDatasets(datasets, load_details=(details_1, details_2, details_3),
+                                        required_target=required_target)
 
     # ------------------
     # Tests
@@ -38,17 +40,20 @@ def test_data_loading():
     assert all(dataset.name in combined_dataset._data for dataset in datasets)
 
     # Check shapes of numpy arrays
-    subjects_1, _, time_steps_1 = combined_dataset._data[MPILemon().name].shape
-    subjects_2, _, time_steps_2 = combined_dataset._data[Miltiadous().name].shape
-    subjects_3, _, time_steps_3 = combined_dataset._data[HatlestadHall().name].shape
+    subjects_1, *_, time_steps_1 = combined_dataset._data[MPILemon().name].shape
+    subjects_2, *_, time_steps_2 = combined_dataset._data[Miltiadous().name].shape
+    subjects_3, *_, time_steps_3 = combined_dataset._data[HatlestadHall().name].shape
 
-    assert (subjects_1, time_steps_1) == (9, 1_003), "Wrong shape of numpy array"
-    assert (subjects_2, time_steps_2) == (14, 305), "Wrong shape of numpy array"
-    assert (subjects_3, time_steps_3) == (3, 975), "Wrong shape of numpy array"
+    assert subjects_1 == 9, "Wrong number of subjects"
+    assert subjects_2 == 14, "Wrong number of subjects"
+    assert subjects_3 == 3, "Wrong number of subjects"
+
+    assert time_steps_1 == time_steps_2 == time_steps_3, "Unequal number of time steps"
 
 
 @pytest.mark.skipif(not os.path.isdir(get_numpy_data_storage_path()), reason="Required numpy arrays not available")
-def test_get_data():
+@pytest.mark.parametrize("required_target", [None, "age", "sex"])
+def test_get_data(required_target):
     """Tests for the get_data method"""
     # ------------------
     # Define the datasets to test
@@ -57,10 +62,10 @@ def test_get_data():
     datasets = (MPILemon(), Miltiadous(), HatlestadHall())
 
     # Defining load details
-    details_1 = LoadDetails(subject_ids=MPILemon().get_subject_ids()[:9], num_time_steps=1_003)
-    details_2 = LoadDetails(subject_ids=Miltiadous().get_subject_ids()[:14], num_time_steps=305)
-    details_3 = LoadDetails(subject_ids=HatlestadHall().get_subject_ids()[:3], time_series_start=1_000,
-                            num_time_steps=975)
+    version = "preprocessed_2024-05-13_173548/data_band_pass_8-12_autoreject_True_sampling_multiple_4"
+    details_1 = LoadDetails(subject_ids=MPILemon().get_subject_ids()[:9], pre_processed_version=version)
+    details_2 = LoadDetails(subject_ids=Miltiadous().get_subject_ids()[:14], pre_processed_version=version)
+    details_3 = LoadDetails(subject_ids=HatlestadHall().get_subject_ids()[:3], pre_processed_version=version)
 
     # ------------------
     # Load all data
@@ -98,8 +103,10 @@ def test_get_data():
         "Wrong type (expected numpy.ndarray)"
 
     # Shape check of values
-    subjects_1, _, time_steps_1 = extracted_data[Miltiadous().name].shape
-    subjects_2, _, time_steps_2 = extracted_data[HatlestadHall().name].shape
+    subjects_1, *_, time_steps_1 = extracted_data[Miltiadous().name].shape
+    subjects_2, *_, time_steps_2 = extracted_data[HatlestadHall().name].shape
 
-    assert (subjects_1, time_steps_1) == (10, 305), "Wrong shape"
-    assert (subjects_2, time_steps_2) == (3, 975), "Wrong shape"
+    assert subjects_1 == 10, "Wrong number of subjects"
+    assert subjects_2 == 3, "Wrong number of subjects"
+
+    assert time_steps_1 == time_steps_2, "Unequal number of time steps"
