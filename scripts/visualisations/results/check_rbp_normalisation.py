fc52154ca7fc7d4ccb79132e860e2f125ca25cb5
thomas <thomas.tveitstol@gmail.com>
2024-09-11 15:08:09 +0200

    improvements to script

--- scripts/visualisations/results/check_rbp_normalisation.py
+++ scripts/visualisations/results/check_rbp_normalisation.py
@@ -3,8 +3,10 @@ I discovered that the normalise_region_representations key in the config file wa
 single element, "true". This script confirmed that RBP always used normalisation.
 
 Output (executed 11th of September 2024):
-Number of RBP models with normalisation: 468
+Number of RBP models with normalisation: 543
 Number of RBP models without normalisation: 0
+Number of interpolation models with normalisation: 263
+Number of interpolation models without normalisation: 282
 """
 import os
 
@@ -15,9 +17,12 @@ from cdl_eeg.data.results_analysis import get_all_ilodo_runs, get_all_lodo_runs,
 def main():
     results_dir = get_results_dir()
 
-    runs = get_all_lodo_runs(results_dir) + get_all_ilodo_runs(results_dir)
-    normalisation_count_true = 0
-    normalisation_count_false = 0
+    runs = (get_all_lodo_runs(results_dir, successful_only=False)
+            + get_all_ilodo_runs(results_dir, successful_only=False))
+    rbp_normalisation_count_true = 0
+    rbp_normalisation_count_false = 0
+    interpolation_normalisation_count_true = 0
+    interpolation_normalisation_count_false = 0
     for run in runs:
         # Get config file
         config_path = os.path.join(results_dir, run)
@@ -30,19 +35,28 @@ def main():
 
             assert isinstance(normalisation, bool), f"Unexpected type: {type(normalisation)}"
             if normalisation:
-                normalisation_count_true += 1
+                rbp_normalisation_count_true += 1
             else:
-                normalisation_count_false += 1
+                rbp_normalisation_count_false += 1
         elif method == "Interpolation":
-            continue
+            normalisation = config["DL Architecture"]["normalise"]
+
+            assert isinstance(normalisation, bool), f"Unexpected type: {type(normalisation)}"
+            if normalisation:
+                interpolation_normalisation_count_true += 1
+            else:
+                interpolation_normalisation_count_false += 1
         else:
             raise ValueError(f"Unexpected method for handling varied numbers of channels: {method}")
 
     # --------------
     # Print the results
     # --------------
-    print(f"Number of RBP models with normalisation: {normalisation_count_true}")
-    print(f"Number of RBP models without normalisation: {normalisation_count_false}")
+    print(f"Number of RBP models with normalisation: {rbp_normalisation_count_true}")
+    print(f"Number of RBP models without normalisation: {rbp_normalisation_count_false}")
+
+    print(f"Number of interpolation models with normalisation: {interpolation_normalisation_count_true}")
+    print(f"Number of interpolation models without normalisation: {interpolation_normalisation_count_false}")
 
 
 if __name__ == "__main__":
