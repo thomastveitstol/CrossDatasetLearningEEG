381b5ef858667b1b6e52537311d11fd5ce70b3ba
thomas <thomas.tveitstol@gmail.com>
2024-11-28 18:46:05 +0100

    added refitting of intercept for LODI

--- scripts/visualisations/results/ilodo/performance_tables/performance_tables_ilodo.py
+++ scripts/visualisations/results/ilodo/performance_tables/performance_tables_ilodo.py
@@ -5,10 +5,13 @@ from typing import Dict
 
 import numpy
 import pandas
+import torch
 
+from cdl_eeg.data.datasets.getter import get_dataset
 from cdl_eeg.data.paths import get_results_dir
 from cdl_eeg.data.analysis.results_analysis import get_all_ilodo_runs, higher_is_better, is_better, \
     get_ilodo_val_dataset_name, PRETTY_NAME
+from cdl_eeg.models.metrics import Histories
 
 
 # ----------------
@@ -60,7 +63,85 @@ def _get_lodo_val_metrics(path, *, main_metric):
     return val_metric, best_epoch, dataset_name
 
 
-def _get_ilodo_test_metrics(path, epoch):
+# ----------------
+# Functions for refitting intercept
+# ----------------
+def _estimate_intercept(df: pandas.DataFrame):
+    # Intercept is calculated as sum(y_i - x_i) / n
+    return (df["ground_truth"] - df["pred"]).mean()
+
+
+def _get_average_prediction(df: pandas.DataFrame, epoch):
+    new_df = {"dataset": df["dataset"], "sub_id": df["sub_id"],
+              "pred": df.iloc[:, (5 * epoch + 2):(5 * epoch + 7)].mean(axis=1)}
+    return pandas.DataFrame.from_dict(data=new_df)
+
+
+def _get_ilodo_refit_scores(path, epoch, metrics) -> Dict[str, Dict[str, float]]:
+    test_predictions = pandas.read_csv(os.path.join(path, "test_history_predictions.csv"))
+    test_metrics = dict()
+
+    # ------------
+    # Loop through all datasets
+    # ------------
+    datasets = set(test_predictions["dataset"])
+    for dataset in datasets:
+        # Get the predictions for this dataset only
+        df = test_predictions[test_predictions["dataset"] == dataset].copy()
+
+        # Average the predictions per EEG epoch
+        df = _get_average_prediction(df=df, epoch=epoch)
+
+        # Add the targets (age)
+        target = "age"  # quick-fixed hard coding
+        df["ground_truth"] = get_dataset(dataset).load_targets(target=target, subject_ids=df["sub_id"])
+
+        # Estimate the intercept
+        new_intercept = _estimate_intercept(df=df)
+        df["adjusted_pred"] = df["pred"] + new_intercept
+
+        # Add the performance
+        test_metrics[dataset] = dict()
+        for metric in metrics:
+            # Normally, I'd add a 'compute_metric' method to Histories, but I don't like to change the code too much after
+            # getting the results from a scientific paper, even when it makes sense. So, violating some best practice
+            # instead
+            test_metrics[dataset][metric] = Histories._compute_metric(
+                metric=metric, y_pred=torch.tensor(df["adjusted_pred"]), y_true=torch.tensor(df["ground_truth"])
+            )
+
+    # ------------
+    # Now, fix the pooled dataset
+    # ------------
+    df = test_predictions
+
+    # Average the predictions per EEG epoch
+    df = _get_average_prediction(df=df, epoch=epoch)
+
+    # Sorting makes life easier
+    df.sort_values(by=["dataset"], inplace=True)
+
+    # Add the targets (age)
+    target = "age"  # quick-fixed hard coding
+    ground_truths = []
+    for dataset in datasets:
+        ground_truths.append(
+            get_dataset(dataset).load_targets(target=target, subject_ids=df["sub_id"][df["dataset"] == dataset])
+        )
+    df["ground_truth"] = numpy.concatenate(ground_truths)
+    new_intercept = _estimate_intercept(df=df)
+    df["adjusted_pred"] = df["pred"] + new_intercept
+
+    # Add the performance
+    test_metrics["Pooled"] = dict()
+    for metric in metrics:
+        test_metrics["Pooled"][metric] = Histories._compute_metric(
+            metric=metric, y_pred=torch.tensor(df["adjusted_pred"]), y_true=torch.tensor(df["ground_truth"])
+        )
+    return test_metrics
+
+
+def _get_ilodo_test_metrics(path, epoch, refit_intercept):
     # -----------------
     # Get the test metrics per test dataset
     # -----------------
@@ -69,18 +150,21 @@ def _get_ilodo_test_metrics(path, epoch):
     metrics = os.listdir(subgroup_path)
 
     # Get all metrics
-    test_metrics = {}
-    for metric in metrics:
-        df = pandas.read_csv(os.path.join(subgroup_path, metric, f"test_{metric}.csv"))
+    if refit_intercept:
+        test_metrics = _get_ilodo_refit_scores(path, epoch, metrics)
+    else:
+        test_metrics = {}
+        for metric in metrics:
+            df = pandas.read_csv(os.path.join(subgroup_path, metric, f"test_{metric}.csv"))
 
-        # Loop through all the test datasets
-        datasets = df.columns
-        for dataset in datasets:
-            if dataset not in test_metrics:
-                test_metrics[dataset] = {}
+            # Loop through all the test datasets
+            datasets = df.columns
+            for dataset in datasets:
+                if dataset not in test_metrics:
+                    test_metrics[dataset] = {}
 
-            # Add the test performance
-            test_metrics[dataset][metric] = df[dataset][epoch]
+                # Add the test performance
+                test_metrics[dataset][metric] = df[dataset][epoch]
 
     # -----------------
     # Add the test metrics on the pooled dataset
@@ -91,7 +175,7 @@ def _get_ilodo_test_metrics(path, epoch):
     return test_metrics
 
 
-def get_best_ilodo_performances(results_dir, *, main_metric, metrics_to_print, verbose):
+def get_best_ilodo_performances(results_dir, *, main_metric, metrics_to_print, verbose, refit_intercept):
     # Get all runs for inverted LODO
     runs = get_all_ilodo_runs(results_dir)
 
@@ -138,7 +222,8 @@ def get_best_ilodo_performances(results_dir, *, main_metric, metrics_to_print, v
     # E.g., {"TDBrain": {"LEMON": {"mse": 10.4, "mae": 2.7}}}
     test_performances: Dict[str, Dict[str, Dict[str, float]]] = {}
     for train_dataset, model in best_models.items():
-        test_performances[train_dataset] = _get_ilodo_test_metrics(path=model.path, epoch=model.best_epoch)
+        test_performances[train_dataset] = _get_ilodo_test_metrics(path=model.path, epoch=model.best_epoch,
+                                                                   refit_intercept=refit_intercept)
 
     # --------------
     # Generate dataframe from the results
@@ -188,23 +273,26 @@ def main():
     verbose = False
 
     # Print results
-    for selection_metric in selection_metrics:
-        print(f"\n\n{f' Selection metric: {selection_metric} ':=^50}\n")
-        df, dfs = get_best_ilodo_performances(
-            results_dir=get_results_dir(), main_metric=selection_metric, metrics_to_print=all_metrics, verbose=verbose
-        )
+    for refit_intercept in (True, False):
+        for selection_metric in selection_metrics:
+            print(f"\n\n{f' Selection metric: {selection_metric} ':=^50}\n")
+            df, dfs = get_best_ilodo_performances(
+                results_dir=get_results_dir(), main_metric=selection_metric, metrics_to_print=all_metrics,
+                verbose=verbose, refit_intercept=refit_intercept
+            )
 
-        # Print the results for easy copy/paste into overleaf document (some changes are still needed, but it still
-        # speeds up the process and is less error-prone)
-        for metric, results_table in dfs.items():
+            # Print the results for easy copy/paste into overleaf document (some changes are still needed, but it still
+            # speeds up the process and is less error-prone)
+            for metric, results_table in dfs.items():
+                print("\hline")
+                print(f"\multirow{r'{5}'}{r'{*}'}{r'{'}{PRETTY_NAME[metric]}{r'}'}")
+                results_table.to_csv(sys.stdout, sep="&", header=False)
+
             print("\hline")
-            print(f"\multirow{r'{5}'}{r'{*}'}{r'{'}{PRETTY_NAME[metric]}{r'}'}")
-            results_table.to_csv(sys.stdout, sep="&", header=False)
 
-        print("\hline")
-
-        # Save the results  todo: must add refitting of intercept
-        df.to_csv(os.path.join(os.path.dirname(__file__), f"results_{selection_metric}.csv"))
+            # Save the results  todo: must add refitting of intercept
+            df.to_csv(os.path.join(os.path.dirname(__file__),
+                                   f"results_{selection_metric}_intercept_{refit_intercept}.csv"))
 
 
 if __name__ == "__main__":
